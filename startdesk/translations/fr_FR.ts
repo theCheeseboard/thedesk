023c985f53fe8dbf057dd0624b4fe607460071a8
Victor Tran <vicr12345@gmail.com>
2020-04-11 22:16:30 +1000

    Add translations for startdesk

---
+++ startdesk/translations/fr_FR.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>CrashWidget</name>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="38"/>
+        <source>Oh, Bonkers!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="48"/>
+        <location filename="../crash/crashwidget.cpp" line="31"/>
+        <source>theDesk had a problem and has stopped working.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="73"/>
+        <source>Relaunch theDesk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="83"/>
+        <source>Log Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.cpp" line="34"/>
+        <source>theDesk couldn&apos;t start because of a problem.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SplashWidget</name>
+    <message>
+        <location filename="../splash/splashwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SplashWindow</name>
+    <message>
+        <location filename="../splashwindow.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
