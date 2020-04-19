8bc99f6deed8d556070175d5b97b4a1c6f34666c
Victor Tran <vicr12345@gmail.com>
2020-04-19 12:20:10 +1000

    Add locale plugin translations

---
+++ plugins/LocalePlugin/translations/ha_NE.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ne_NP" sourcelanguage="en_US">
+<context>
+    <name>OnboardingRegion</name>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="35"/>
+        <location filename="../onboarding/onboardingregion.cpp" line="71"/>
+        <source>Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="42"/>
+        <source>Whereabouts are you located? We&apos;ll use this information to format your dates, numbers and other items correctly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="115"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ plugins/LocalePlugin/translations/vi_VN.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi_VN" sourcelanguage="en_US">
+<context>
+    <name>OnboardingRegion</name>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="35"/>
+        <location filename="../onboarding/onboardingregion.cpp" line="71"/>
+        <source>Region</source>
+        <translation>Khu vực</translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="42"/>
+        <source>Whereabouts are you located? We&apos;ll use this information to format your dates, numbers and other items correctly.</source>
+        <translation>Bạn đang ở đâu? Chúng tôi sẽ sử dụng thông tin này để định dạng các ngày, số và điều khác.</translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="115"/>
+        <source>Next</source>
+        <translation>Tiếp</translation>
+    </message>
+</context>
+</TS>
