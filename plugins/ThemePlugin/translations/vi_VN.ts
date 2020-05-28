1ac0c5ffe618fa1bcd9c1b5be4d2efe82c60747e
Victor Tran <vicr12345@gmail.com>
2020-05-28 16:29:59 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 86.6% (26 of 30 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/vi/

--- plugins/ThemePlugin/translations/vi_VN.ts
+++ plugins/ThemePlugin/translations/vi_VN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>OnboardingTheme</name>
     <message>
@@ -12,27 +12,27 @@
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="85"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Giao diện</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chọn một giao diện. Bạn có thể thay đổi điều này bất cứ lúc nào.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Tối</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="108"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Sáng</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="238"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="70"/>
@@ -56,107 +56,107 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="132"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Giao diện</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Tối</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Sáng</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Thay đổi màu giao diện</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>MÀU</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="200"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Phông chữ đơn cách</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="207"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Phông chữ giao diện</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="230"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>PHÔNG</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="296"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Giao diện thành phần</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="360"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Nút</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="372"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Nút chọn 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="382"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng giao diện được đặt ở trên, các thành phần sẽ hiện như thế này:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="389"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Nút chọn 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="396"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Hộp kiểm 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="412"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>BẢN XEM TRƯỚC</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="419"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Hộp kiểm 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="433"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhập ở đây!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="289"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt sự xuất hiện cho nút, hộp văn bản và các thành phần giao diện</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="309"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>THÀNH PHẦN GIAO DIỆN</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Màu nền</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Màu sắc</translation>
     </message>
 </context>
 </TS>
