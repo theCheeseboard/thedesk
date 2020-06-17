97641aceacd54cbefc9d29332ee3513418717e78
Victor Tran <vicr12345@gmail.com>
2020-06-17 14:52:44 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: theDesk/theDesk Polkit
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-polkit/vi/

--- polkitagent/translations/vi_VN.ts
+++ polkitagent/translations/vi_VN.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>AuthWindow</name>
     <message>
         <location filename="../authwindow.ui" line="35"/>
         <source>PolicyKit Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Xác thực PolicyKit</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="199"/>
         <source>Enter your password to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Điền mật khẩu của bạn để xác nhận hoạt động này</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="441"/>
         <source>Pass the authentication challenge to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Trả lời thử thách xác thực để xác nhận hoạt động này</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="159"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Lỗi</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="166"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông tin</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../identitypopover.ui" line="35"/>
         <source>Identity</source>
-        <translation type="unfinished"></translation>
+        <translation>Danh tính</translation>
     </message>
     <message>
         <location filename="../identitypopover.ui" line="42"/>
         <source>Who do you want to authenticate as?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn muốn xác thực bằng ai?</translation>
     </message>
 </context>
 </TS>
