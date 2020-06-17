c3d5d3652cf3ff411e79b038d3b7ce7046c31822
Victor Tran <vicr12345@gmail.com>
2020-06-17 20:56:58 +1000

    Try fixing polkit autostart

--- startdesk/splash/splashcontroller.cpp
+++ startdesk/splash/splashcontroller.cpp
@@ -111,9 +111,9 @@ void SplashController::runAutostart() {
 
     //Autostart the Polkit agent
     QString pkPath = QStringLiteral(SYSTEM_LIBRARY_DIRECTORY).append("/td-polkitagent");
-    if (QFile::exists(pkPath)) {
-        QProcess::startDetached(pkPath, QStringList());
-    }
+//    if (QFile::exists(pkPath)) {
+    QProcess::startDetached(pkPath, QStringList());
+//    }
 
     QStringList searchPaths = {
         qEnvironmentVariable("XDG_CONFIG_HOME", QDir::homePath() + "/.config") + "/autostart",
