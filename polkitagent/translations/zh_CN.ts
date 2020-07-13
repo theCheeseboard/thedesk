d8c1641c8b8c51a38d691d8ff8e9582451e93f19
Oscar <blue@bluexguardian.com>
2020-07-13 11:46:35 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: theDesk/theDesk Polkit
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-polkit/zh_Hans/

--- polkitagent/translations/zh_CN.ts
+++ polkitagent/translations/zh_CN.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>AuthWindow</name>
     <message>
         <location filename="../authwindow.ui" line="35"/>
         <source>PolicyKit Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>策略配置验证</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="199"/>
         <source>Enter your password to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>输入你的密码以确定</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="441"/>
         <source>Pass the authentication challenge to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>通过此挑战来确定此行为</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="159"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="166"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>信息</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../identitypopover.ui" line="35"/>
         <source>Identity</source>
-        <translation type="unfinished"></translation>
+        <translation>身份</translation>
     </message>
     <message>
         <location filename="../identitypopover.ui" line="42"/>
         <source>Who do you want to authenticate as?</source>
-        <translation type="unfinished"></translation>
+        <translation>你想作为谁来进行认证？</translation>
     </message>
 </context>
 </TS>
