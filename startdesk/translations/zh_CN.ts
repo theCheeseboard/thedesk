f44439067349c129e9ad070013337eb02c747dec
Oscar <blue@bluexguardian.com>
2020-07-13 11:51:34 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: theDesk/theDesk Session
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-session/zh_Hans/

--- startdesk/translations/zh_CN.ts
+++ startdesk/translations/zh_CN.ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>CrashWidget</name>
     <message>
         <location filename="../crash/crashwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>表格</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="38"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>糟糕！！</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="48"/>
         <location filename="../crash/crashwidget.cpp" line="31"/>
         <source>theDesk had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk遇到了错误并停止了运行。</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="73"/>
         <source>Relaunch theDesk</source>
-        <translation type="unfinished"></translation>
+        <translation>重启theDesk</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="83"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>注销</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.cpp" line="34"/>
         <source>theDesk couldn&apos;t start because of a problem.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk 因为某种原因不能启动。</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../splash/splashwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>表格</translation>
     </message>
 </context>
 <context>
@@ -48,22 +48,22 @@
     <message>
         <location filename="../splashwindow.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>对话框</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="104"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>问题</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="118"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>是</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="128"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>否</translation>
     </message>
 </context>
 </TS>
