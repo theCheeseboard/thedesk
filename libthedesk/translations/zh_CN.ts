386597b3f521514267aea5d18b56133d84792131
Oscar <blue@bluexguardian.com>
2020-07-13 11:39:38 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: theDesk/theDesk Core
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-core/zh_Hans/

--- libthedesk/translations/zh_CN.ts
+++ libthedesk/translations/zh_CN.ts
@@ -1,50 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>LocaleSelector</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>表格</translation>
     </message>
     <message>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>选择地区</translation>
     </message>
 </context>
 <context>
     <name>QuietModeManager</name>
     <message>
         <source>Allows all sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>允许所有的声音</translation>
     </message>
     <message>
         <source>Hides non-critical notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>隐藏不重要的通知</translation>
     </message>
     <message>
         <source>Hides all notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>隐藏所有通知</translation>
     </message>
     <message>
         <source>Mutes all sound altogether</source>
-        <translation type="unfinished"></translation>
+        <translation>开启全局静音</translation>
     </message>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>声音</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>限紧急</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>无通知</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>静音</translation>
     </message>
 </context>
 </TS>
