14ee91c8150621387f2e2e8e9874d57dff54da8d
Oscar <blue@bluexguardian.com>
2020-07-13 11:38:29 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: theDesk/theDesk Audio
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-audio/zh_Hans/

--- plugins/AudioPlugin/translations/zh_CN.ts
+++ plugins/AudioPlugin/translations/zh_CN.ts
@@ -1,31 +1,31 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>EventHandler</name>
     <message>
         <source>No Audio Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>未找到音频设备</translation>
     </message>
     <message>
         <source>Speakers</source>
-        <translation type="unfinished"></translation>
+        <translation>音响</translation>
     </message>
     <message>
         <source>Headphones</source>
-        <translation type="unfinished"></translation>
+        <translation>耳机</translation>
     </message>
     <message>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>音量</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>静音</translation>
     </message>
     <message>
         <source>Unmute Quiet Mode before changing the volume</source>
-        <translation type="unfinished"></translation>
+        <translation>在更改音量之前请解除安静模式</translation>
     </message>
 </context>
 </TS>
