c02a18427a51ea38fcef0e2b6254ad2d29c25f49
Oscar <blue@bluexguardian.com>
2020-07-13 11:54:48 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: theDesk/theDesk Overview
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-overview/zh_Hans/

--- plugins/OverviewPlugin/translations/zh_CN.ts
+++ plugins/OverviewPlugin/translations/zh_CN.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>OverviewPane</name>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="106"/>
         <source>Hi %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>你好 %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="108"/>
         <source>Good morning, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>早上好， %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="110"/>
         <source>Good afternoon, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>下午好， %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="112"/>
         <source>Good evening, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>晚上好， %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="254"/>
         <source>Overview</source>
-        <translation type="unfinished"></translation>
+        <translation>总览</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.ui" line="102"/>
         <source>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</source>
-        <translation type="unfinished"></translation>
+        <translation>希望你有个很棒的一天。 这里是我为你收集的东西：</translation>
     </message>
 </context>
 </TS>
