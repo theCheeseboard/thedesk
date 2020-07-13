327bfc1fad6f55443aa76e2aa47be8918727753d
Oscar <blue@bluexguardian.com>
2020-07-13 14:01:00 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: theDesk/theDesk Brightness
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-brightness/sk/

--- plugins/BrightnessPlugin/translations/sk_SK.ts
+++ plugins/BrightnessPlugin/translations/sk_SK.ts
@@ -9,7 +9,7 @@
         <location filename="../plugin.cpp" line="88"/>
         <location filename="../plugin.cpp" line="94"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>jas</translation>
     </message>
 </context>
 <context>
@@ -17,7 +17,7 @@
     <message>
         <location filename="../screenbrightnesschunk.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>formulár</translation>
     </message>
 </context>
 </TS>
