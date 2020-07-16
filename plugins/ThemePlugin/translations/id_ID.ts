45b724dbe387fd98cc3d4c7ce3a46653f1ca32fd
Zumi <daxuya.zumi+code@protonmail.com>
2020-07-16 05:02:08 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 84.8% (28 of 33 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/id/

--- plugins/ThemePlugin/translations/id_ID.ts
+++ plugins/ThemePlugin/translations/id_ID.ts
@@ -7,12 +7,12 @@
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="91"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilih tema. Anda dapat menggantinya kapan saja.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
@@ -46,132 +46,132 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="198"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Gelap</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Terang</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengganti warna umum desktop</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>WARNA</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="224"/>
         <source>WINDOW BORDERS</source>
-        <translation type="unfinished"></translation>
+        <translation>DEKORASI JENDELA</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="231"/>
         <source>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jika dekorasi tidak sesuai dengan tema yang Anda pilih, kami dapat memperbaikinya. Ini akan mengganti dekorasi jendela pada KDE serta desktop apapun yang menggunakan KWin sebagai manajer jendela.</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="243"/>
         <source>Set Window Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Terapkan</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="274"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font Kode</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="281"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font Antarmuka</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="304"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>FONT</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="370"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema Widget</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="434"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Tombol</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="447"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Tombol Pilihan 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="457"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Dengan tema di atas, elemen antarmuka akan tampil seperti ini:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="464"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Tombol Pilihan 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="471"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Centang 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="487"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>DEMO</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="494"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Centang 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="508"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ketik di sini!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="543"/>
         <source>Transparency Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Efek Transparan</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="563"/>
         <source>EFFECTS</source>
-        <translation type="unfinished"></translation>
+        <translation>EFEK</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="363"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengatur tampilan tombol, kotak teks dan elemen antarmuka lainnya</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="383"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>WIDGET</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Warna Dasar</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Warna Aksen</translation>
     </message>
 </context>
 </TS>
