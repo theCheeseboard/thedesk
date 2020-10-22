cddcc810c6b0a04d0f6a8e1cff35d3ae1496c323
Atahan Reis <atahan.r@outlook.com>
2020-10-22 01:42:58 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: theDesk/theDesk Session
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-session/tr/

--- startdesk/translations/tr_TR.ts
+++ startdesk/translations/tr_TR.ts
@@ -6,33 +6,33 @@
     <message>
         <location filename="../crash/crashwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="38"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>Oh, Bir Kaçık!</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="48"/>
         <location filename="../crash/crashwidget.cpp" line="31"/>
         <source>theDesk had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk&apos;te bir sorun vardı ve çalışmayı durdurdu.</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="73"/>
         <source>Relaunch theDesk</source>
-        <translation type="unfinished"></translation>
+        <translation>TheDesk&apos;i yeniden başlatın</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="83"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Çıkış Yap</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.cpp" line="34"/>
         <source>theDesk couldn&apos;t start because of a problem.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk bir sorun nedeniyle başlayamadı.</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../splash/splashwidget.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
 </context>
 <context>
@@ -48,22 +48,22 @@
     <message>
         <location filename="../splashwindow.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Diyalog</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="104"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Soru</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="118"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Evet</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="128"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Yok</translation>
     </message>
 </context>
 </TS>
