194912ee601e91874d29f668abd6cd3405d226ee
Atahan Reis <atahan.r@outlook.com>
2020-10-22 02:09:12 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: theDesk/theDesk Locale
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-locale/tr/

--- plugins/LocalePlugin/translations/tr_TR.ts
+++ plugins/LocalePlugin/translations/tr_TR.ts
@@ -7,107 +7,107 @@
         <location filename="../settings/localesettingspane.ui" line="35"/>
         <location filename="../settings/localesettingspane.cpp" line="119"/>
         <source>Locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Yerel</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="107"/>
         <source>LANGUAGE</source>
-        <translation type="unfinished"></translation>
+        <translation>DİL</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="114"/>
         <source>Prioritise the languages you&apos;d like to use on your device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cihazınızda kullanmak istediğiniz dillere öncelik verin.</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="133"/>
         <source>Add Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Dil Ekle</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="277"/>
         <source>Currency</source>
-        <translation type="unfinished"></translation>
+        <translation>Para birimi</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="221"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaman</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="242"/>
         <source>Measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Ölçüm</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="263"/>
         <source>Country</source>
-        <translation type="unfinished"></translation>
+        <translation>Ülke</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="184"/>
         <source>Select your preferred regional formats</source>
-        <translation type="unfinished"></translation>
+        <translation>Tercih ettiğiniz bölgesel biçimleri seçin</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="177"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Sayılar</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="228"/>
         <source>First day of week</source>
-        <translation type="unfinished"></translation>
+        <translation>Haftanın ilk günü</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="207"/>
         <source>REGION</source>
-        <translation type="unfinished"></translation>
+        <translation>BÖLGE</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="256"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarih</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="78"/>
         <source>Primary</source>
-        <translation type="unfinished"></translation>
+        <translation>Birincil</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="95"/>
         <source>Metric</source>
-        <translation type="unfinished"></translation>
+        <translation>Metrik</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="98"/>
         <source>Imperial</source>
-        <translation type="unfinished"></translation>
+        <translation>İmparatorluk</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="101"/>
         <source>Imperial (United Kingdom)</source>
-        <translation type="unfinished"></translation>
+        <translation>İmparatorluk (Birleşik Krallık)</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="152"/>
         <source>For this language</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu dil için</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="153"/>
         <source>Move Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Yukarı Taşı</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="156"/>
         <source>Move Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Aşağı inmek</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="159"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaldırmak</translation>
     </message>
 </context>
 <context>
@@ -116,17 +116,17 @@
         <location filename="../onboarding/onboardingregion.ui" line="35"/>
         <location filename="../onboarding/onboardingregion.cpp" line="60"/>
         <source>Region</source>
-        <translation type="unfinished"></translation>
+        <translation>Bölge</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingregion.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to format your dates, numbers and other items correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nerelerde bulunuyorsunuz? Bu bilgileri tarihlerinizi, sayılarınızı ve diğer öğelerinizi doğru şekilde biçimlendirmek için kullanacağız.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingregion.ui" line="115"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonraki</translation>
     </message>
 </context>
 </TS>
