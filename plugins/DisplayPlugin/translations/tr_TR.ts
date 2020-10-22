18ca9b917b1e343dbd74efdd8940e795bcc1568a
Atahan Reis <atahan.r@outlook.com>
2020-10-22 15:16:12 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: theDesk/theDesk Display
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-display/tr/

--- plugins/DisplayPlugin/translations/tr_TR.ts
+++ plugins/DisplayPlugin/translations/tr_TR.ts
@@ -5,128 +5,128 @@
     <name>ArrangeWidget</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>DISPLAY PROPERTIES</source>
-        <translation type="unfinished"></translation>
+        <translation>GÖRÜNTÜ ÖZELLİKLERİ</translation>
     </message>
     <message>
         <source>Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekran boyutu</translation>
     </message>
     <message>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Yenileme hızı</translation>
     </message>
     <message>
         <source>%1 hz</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 hz</translation>
     </message>
     <message>
         <source>Apply Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarları uygula</translation>
     </message>
     <message>
         <source>Primary Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Birincil Ekran</translation>
     </message>
 </context>
 <context>
     <name>DisplaySettings</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Görüntüler</translation>
     </message>
     <message>
         <source>REDSHIFT</source>
-        <translation type="unfinished"></translation>
+        <translation>KIRMIZIÜSTKARAKTER</translation>
     </message>
     <message>
         <source>Redshift blocks blue light so that you can sleep better at night</source>
-        <translation type="unfinished"></translation>
+        <translation>Kirmizi Üst karakter geceleri daha iyi uyuyabilmeniz için mavi ışığı engeller</translation>
     </message>
     <message>
         <source>Schedule Redshift</source>
-        <translation type="unfinished"></translation>
+        <translation>Kirmizi Üstkarakter&apos;i planlayın</translation>
     </message>
     <message>
         <source>Intensity</source>
-        <translation type="unfinished"></translation>
+        <translation>Yoğunluk</translation>
     </message>
     <message>
         <source>Follow Sunlight Cycle</source>
-        <translation type="unfinished"></translation>
+        <translation>Sunlight Cycle&apos;ı Takip Edin</translation>
     </message>
     <message>
         <source>Start at</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlamak</translation>
     </message>
     <message>
         <source>End at</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitiş noktasında</translation>
     </message>
     <message>
         <source>SCREENS</source>
-        <translation type="unfinished"></translation>
+        <translation>EKRANLAR</translation>
     </message>
     <message>
         <source>Change the resolution of your display and arrange your displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekranınızın çözünürlüğünü değiştirin ve ekranlarınızı düzenleyin</translation>
     </message>
     <message>
         <source>Arrange and Resize Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekranları Düzenleme ve Yeniden Boyutlandırma</translation>
     </message>
     <message>
         <source>Your physical location will be sent to MET Norway to determine sunrise and sunset times.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fiziksel konumunuz, gün doğumu ve gün batımı saatlerini belirlemek için MET Norveç&apos;e gönderilecektir.</translation>
     </message>
     <message>
         <source>SCALING</source>
-        <translation type="unfinished"></translation>
+        <translation>ÖLÇEKLENDİRME</translation>
     </message>
     <message>
         <source>If items on the screen are too small, you can scale them up to make them easier to see.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekrandaki öğeler çok küçükse, daha kolay görülmelerini sağlamak için ölçeklerini büyütebilirsiniz.</translation>
     </message>
     <message>
         <source>100% (96 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>100% (96 DPI)</translation>
     </message>
     <message>
         <source>125% (120 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>125% (120 DPI)</translation>
     </message>
     <message>
         <source>150% (144 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>150% (144 DPI)</translation>
     </message>
     <message>
         <source>200% (192 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>200% (192 DPI)</translation>
     </message>
 </context>
 <context>
     <name>OverlayWindow</name>
     <message>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Diyalog</translation>
     </message>
 </context>
 <context>
     <name>RedshiftDaemon</name>
     <message>
         <source>Redshift Active</source>
-        <translation type="unfinished"></translation>
+        <translation>Kırmızı ÜstKarakter Aktif</translation>
     </message>
     <message>
         <source>Disable Redshift until tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Kırmızı ÜstKarakter&apos;i yarına kadar devre dışı bırakın</translation>
     </message>
 </context>
 </TS>
