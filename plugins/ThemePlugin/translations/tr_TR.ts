706ebe27cab6ddb7892d4e08095ba1c9bdce2f61
Atahan Reis <atahan.r@outlook.com>
2020-10-22 18:28:04 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/tr/

--- plugins/ThemePlugin/translations/tr_TR.ts
+++ plugins/ThemePlugin/translations/tr_TR.ts
@@ -7,37 +7,37 @@
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="91"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir tema seç. Bunu istediğiniz zaman değiştirebileceksiniz.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Karanlık</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="108"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Aydınlık</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="238"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonraki</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="76"/>
         <source>Intellect has spoken; blinding shall be no more!</source>
-        <translation type="unfinished"></translation>
+        <translation>Akıl konuştu; kör edici olmayacak artık!</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="81"/>
         <source>One whose wisdom has brought them the light of day!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hikmeti onlara gün ışığını getiren biri!</translation>
     </message>
 </context>
 <context>
@@ -46,132 +46,132 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="198"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Karanlık</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Aydınlık</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Arayüzün renklerini değiştirin</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>RENKLER</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="224"/>
         <source>WINDOW BORDERS</source>
-        <translation type="unfinished"></translation>
+        <translation>PENCERE KENARLARI</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="231"/>
         <source>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pencere kenarlıkları doğru görünmüyorsa, bunu sizin için düzeltebiliriz. Bu aynı zamanda KDE&apos;deki pencere kenarlıklarınızı ve pencere yöneticisi için KWin kullanan diğer masaüstlerini de değiştirecektir.</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="243"/>
         <source>Set Window Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Pencere Kenarlıklarını Ayarlama</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="274"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Sabit Genişlikli Yazı Tipi</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="281"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Arayüz Yazı Tipi</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="304"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>FONTLAR</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="370"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Widget Teması</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="434"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Buton</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="447"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Radyo Düğmesi 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="457"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Yukarıdaki tema kümesini kullandığınızda, arayüz öğeleri şöyle görünecektir:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="464"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Radyo Düğmesi 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="471"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Onay Kutusu 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="487"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>GÖSTERİM</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="494"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Onay Kutusu 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="508"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Buraya yaz!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="543"/>
         <source>Transparency Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Şeffaflık Etkileri</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="563"/>
         <source>EFFECTS</source>
-        <translation type="unfinished"></translation>
+        <translation>ÖZEL EFEKTLER</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="363"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Düğmelerin, metin kutularının ve diğer arayüz öğelerinin görünümünü ayarlayın</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="383"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>WIDGET&apos;LAR</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Temel Renk</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Vurgu Rengi</translation>
     </message>
 </context>
 </TS>
