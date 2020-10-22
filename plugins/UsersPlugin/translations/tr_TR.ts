1c04030bfab75f6fc984c2d3b1c1c37379217f07
Atahan Reis <atahan.r@outlook.com>
2020-10-22 21:56:52 +0000

    Translated using Weblate (Turkish)
    Currently translated at 70.6% (65 of 92 strings)
    
    Translation: theDesk/theDesk Users
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-users/tr/

--- plugins/UsersPlugin/translations/tr_TR.ts
+++ plugins/UsersPlugin/translations/tr_TR.ts
@@ -8,165 +8,165 @@
         <location filename="../settings/adduserdialog.ui" line="629"/>
         <location filename="../settings/adduserdialog.ui" line="745"/>
         <source>Add User</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı Ekle</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="161"/>
         <source>PERSONAL DETAILS</source>
-        <translation type="unfinished"></translation>
+        <translation>KİŞİSEL DETAYLAR</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="168"/>
         <location filename="../settings/adduserdialog.ui" line="688"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı adı</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="175"/>
         <source>Full Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ad ve Soyad</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="188"/>
         <source>The username is generated based on the given full name. We recommend not changing it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı adı, verilen tam ada göre oluşturulur. Değiştirmemenizi öneririz.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="245"/>
         <source>ADMINISTRATOR</source>
-        <translation type="unfinished"></translation>
+        <translation>YÖNETİCİ</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="262"/>
         <location filename="../settings/adduserdialog.cpp" line="187"/>
         <source>Administrator</source>
-        <translation type="unfinished"></translation>
+        <translation>Yönetici</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="293"/>
         <source>STANDARD USER</source>
-        <translation type="unfinished"></translation>
+        <translation>STANDART KULLANICI</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="252"/>
         <source>An administrator has full control over this device. They&apos;ll be able to configure systemwide settings and install software.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir yönetici, bu cihaz üzerinde tam denetime sahiptir. Sistem genelindeki ayarları yapılandırabilecek ve yazılım yükleyebilecekler.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="300"/>
         <source>A standard user can&apos;t perform systemwide actions. They&apos;ll only be able to perform administrative actions that only affect their account.</source>
-        <translation type="unfinished"></translation>
+        <translation>Standart bir kullanıcı, sistem genelinde eylemler gerçekleştiremez. Yalnızca kendi hesaplarını etkileyen idari işlemleri gerçekleştirebilecekler.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="310"/>
         <source>Standard User</source>
-        <translation type="unfinished"></translation>
+        <translation>Standart kullanıcı</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="332"/>
         <source>USER TYPE</source>
-        <translation type="unfinished"></translation>
+        <translation>KULLANICI TİPİ</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="409"/>
         <source>Account Security</source>
-        <translation type="unfinished"></translation>
+        <translation>Hesap Güvenliği</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="434"/>
         <source>Protect this account with a password to prevent anyone else logging into it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Başkalarının oturum açmasını önlemek için bu hesabı bir parola ile koruyun.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="474"/>
         <location filename="../settings/adduserdialog.ui" line="729"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="488"/>
         <source>Confirm Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifreyi Onayla</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="505"/>
         <source>Password Hint</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre ipucu</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="512"/>
         <source>If you forget your password, this will appear to help you remember.</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifrenizi unutursanız, bu hatırlamanıza yardımcı olacak şekilde görünecektir.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="524"/>
         <source>Use this password</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu şifreyi kullanın</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="531"/>
         <source>Alternatively, choose one of the following options:</source>
-        <translation type="unfinished"></translation>
+        <translation>Alternatif olarak, aşağıdaki seçeneklerden birini seçin:</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="541"/>
         <source>Ask for a password on the next login</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir sonraki girişte şifre isteyin</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="544"/>
         <source>The first time the user logs in, we&apos;ll ask them for a password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı ilk kez oturum açtığında, ondan bir parola isteyeceğiz.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="551"/>
         <source>Don&apos;t set a password</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir şifre belirlemeyin</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="554"/>
         <source>Anyone will be able to log into this account.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hesaba herkes giriş yapabilecek.</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="654"/>
         <source>Add this user to your system?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcı sisteminize eklensin mi?</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="695"/>
         <source>Real Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ad</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.ui" line="702"/>
         <source>User Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı tipi</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.cpp" line="103"/>
         <source>Set Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifreyi belirle</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.cpp" line="110"/>
         <source>Ask when logging in</source>
-        <translation type="unfinished"></translation>
+        <translation>Giriş yaparken sor</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.cpp" line="117"/>
         <source>Not Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlanmadı</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.cpp" line="165"/>
         <source>Couldn&apos;t create user</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı oluşturulamadı</translation>
     </message>
     <message>
         <location filename="../settings/adduserdialog.cpp" line="194"/>
         <source>Standard</source>
-        <translation type="unfinished"></translation>
+        <translation>Standart</translation>
     </message>
 </context>
 <context>
@@ -174,83 +174,83 @@
     <message>
         <location filename="../settings/changepassworddialog.ui" line="68"/>
         <source>Change Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre değiştir</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="94"/>
         <source>Set a password to protect this account.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hesabı korumak için bir şifre belirleyin.</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="237"/>
         <source>WARNING</source>
-        <translation type="unfinished"></translation>
+        <translation>UYARI</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="244"/>
         <source>This account is locked. Changing the password will unlock it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hesap kilitlendi. Şifreyi değiştirmek kilidini açacaktır.</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="289"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="303"/>
         <source>Confirm Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifreyi Onayla</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="320"/>
         <source>Password Hint</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre ipucu</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="327"/>
         <source>If you forget your password, this will appear to help you remember.</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifrenizi unutursanız, bu hatırlamanıza yardımcı olacak şekilde görünecektir.</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="339"/>
         <source>Use this password</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu şifreyi kullanın</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="346"/>
         <source>Alternatively, choose one of the following options:</source>
-        <translation type="unfinished"></translation>
+        <translation>Alternatif olarak, aşağıdaki seçeneklerden birini seçin:</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="356"/>
         <source>Ask for a password on the next login</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir sonraki girişte şifre isteyin</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="359"/>
         <source>The first time the user logs in, we&apos;ll ask them for a password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı ilk kez oturum açtığında, ondan bir parola isteyeceğiz.</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="366"/>
         <source>Don&apos;t set a password</source>
-        <translation type="unfinished"></translation>
+        <translation>Parola belirleme</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.ui" line="369"/>
         <source>Anyone will be able to log into this account.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hesaba herkes giriş yapabilecek.</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.cpp" line="71"/>
         <source>Couldn&apos;t set password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre ayarlanamadı</translation>
     </message>
     <message>
         <location filename="../settings/changepassworddialog.cpp" line="89"/>
         <location filename="../settings/changepassworddialog.cpp" line="107"/>
         <source>Couldn&apos;t set password policy</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre ilkesi ayarlanamadı</translation>
     </message>
 </context>
 <context>
@@ -258,27 +258,27 @@
     <message>
         <location filename="../settings/changerealnamedialog.ui" line="68"/>
         <source>Change Real Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ve Adı Değiştir</translation>
     </message>
     <message>
         <location filename="../settings/changerealnamedialog.ui" line="77"/>
         <source>Your Real Name appears on the login screen, on the Gateway and in any other location where we need to differentiate between users.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ve Adınız oturum açma ekranında, Ağ Geçidinde ve kullanıcılar arasında ayrım yapmamız gereken diğer herhangi bir yerde görünür.</translation>
     </message>
     <message>
         <location filename="../settings/changerealnamedialog.ui" line="123"/>
         <source>Real Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ve ad</translation>
     </message>
     <message>
         <location filename="../settings/changerealnamedialog.ui" line="132"/>
         <source>Set Real Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ve Adı Ayarla</translation>
     </message>
     <message>
         <location filename="../settings/changerealnamedialog.cpp" line="64"/>
         <source>Couldn&apos;t set real name</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerçek ad belirlenemedi</translation>
     </message>
 </context>
 <context>
@@ -288,37 +288,37 @@
         <location filename="../settings/deleteuserdialog.ui" line="153"/>
         <location filename="../settings/deleteuserdialog.ui" line="207"/>
         <source>Delete User</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcıyı sil</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="77"/>
         <source>What do you want to do with this user&apos;s files?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcının dosyalarıyla ne yapmak istiyorsunuz?</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="90"/>
         <source>Delete Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosyaları sil</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="93"/>
         <source>Files for this user will be removed from this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcının dosyaları bu cihazdan kaldırılacaktır.</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="100"/>
         <source>Keep Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosyaları sakla</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="103"/>
         <source>Files for this user will be kept in the same location.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcının dosyaları aynı yerde tutulacak.</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="179"/>
         <source>Delete this user from this device?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcı bu cihazdan silinsin mi?</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="189"/>
@@ -326,17 +326,20 @@
 - Home Directory
 - Mail Spool
 - Temporary Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu kullanıcıyla ilişkili tüm veriler sonsuza kadar silinecek. Bu içerir
+- Ana Dizin
+- Posta Biriktirme
+- Geçici dosyalar</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.ui" line="210"/>
         <source>They&apos;ll be gone forever!</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonsuza dek gitmiş olacaklar!</translation>
     </message>
     <message>
         <location filename="../settings/deleteuserdialog.cpp" line="74"/>
         <source>Couldn&apos;t delete user</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı silinemedi</translation>
     </message>
 </context>
 <context>
@@ -345,12 +348,12 @@
         <location filename="../settings/lockuserdialog.ui" line="68"/>
         <location filename="../settings/lockuserdialog.ui" line="90"/>
         <source>Lock Account</source>
-        <translation type="unfinished"></translation>
+        <translation>Kilitli hesap</translation>
     </message>
     <message>
         <location filename="../settings/lockuserdialog.ui" line="77"/>
         <source>After locking this account, you won&apos;t be able to log in as this account.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu hesabı kilitledikten sonra, bu hesapla giriş yapamayacaksınız.</translation>
     </message>
     <message>
         <location filename="../settings/lockuserdialog.ui" line="140"/>
