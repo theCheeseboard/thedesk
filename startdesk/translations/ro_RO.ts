2a76e283538fd0b9d5088541c3b5f7fb9bbc21af
thonkdifferent <thonkdifferent@outlook.com>
2020-12-10 18:04:31 +0000

    Translated using Weblate (Romanian)
    Currently translated at 81.8% (9 of 11 strings)
    
    Translation: theDesk/theDesk Session
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-session/ro/

--- startdesk/translations/ro_RO.ts
+++ startdesk/translations/ro_RO.ts
@@ -11,28 +11,28 @@
     <message>
         <location filename="../crash/crashwidget.ui" line="38"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation>O, vai!</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="48"/>
         <location filename="../crash/crashwidget.cpp" line="31"/>
         <source>theDesk had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk a întâmpinat o problemă și s-a oprit.</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="73"/>
         <source>Relaunch theDesk</source>
-        <translation type="unfinished"></translation>
+        <translation>Repornește theDesk</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="83"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Deconectare</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.cpp" line="34"/>
         <source>theDesk couldn&apos;t start because of a problem.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk nu a putut porni din cauza unei probleme.</translation>
     </message>
 </context>
 <context>
@@ -48,22 +48,22 @@
     <message>
         <location filename="../splashwindow.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="104"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Întrebare</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="118"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Da</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="128"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nu</translation>
     </message>
 </context>
 </TS>
