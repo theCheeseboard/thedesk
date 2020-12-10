bcc32875e3e58fde454aac25d01cbc2461345d83
thonkdifferent <thonkdifferent@outlook.com>
2020-12-10 18:36:32 +0000

    Translated using Weblate (Romanian)
    Currently translated at 89.6% (26 of 29 strings)
    
    Translation: theDesk/theDesk Display
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-display/ro/

--- plugins/DisplayPlugin/translations/ro_RO.ts
+++ plugins/DisplayPlugin/translations/ro_RO.ts
@@ -9,27 +9,27 @@
     </message>
     <message>
         <source>DISPLAY PROPERTIES</source>
-        <translation type="unfinished"></translation>
+        <translation>Proprietățile ecranului</translation>
     </message>
     <message>
         <source>Resolution</source>
-        <translation type="unfinished"></translation>
+        <translation>Rezoluție</translation>
     </message>
     <message>
         <source>Refresh Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Rată de reîmprospătare</translation>
     </message>
     <message>
         <source>%1 hz</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 hz</translation>
     </message>
     <message>
         <source>Apply Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplică setările</translation>
     </message>
     <message>
         <source>Primary Display</source>
-        <translation type="unfinished"></translation>
+        <translation>Monitor principal</translation>
     </message>
 </context>
 <context>
@@ -40,75 +40,75 @@
     </message>
     <message>
         <source>Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Monitoare</translation>
     </message>
     <message>
         <source>REDSHIFT</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift</translation>
     </message>
     <message>
         <source>Redshift blocks blue light so that you can sleep better at night</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift blochează lumina albastră ca să dormiți mai bine noaptea</translation>
     </message>
     <message>
         <source>Schedule Redshift</source>
-        <translation type="unfinished"></translation>
+        <translation>Programați Redshift</translation>
     </message>
     <message>
         <source>Intensity</source>
-        <translation type="unfinished"></translation>
+        <translation>Intensitate</translation>
     </message>
     <message>
         <source>Follow Sunlight Cycle</source>
-        <translation type="unfinished"></translation>
+        <translation>Urmărește ciclul solar</translation>
     </message>
     <message>
         <source>Start at</source>
-        <translation type="unfinished"></translation>
+        <translation>Începe la</translation>
     </message>
     <message>
         <source>End at</source>
-        <translation type="unfinished"></translation>
+        <translation>Se termină la</translation>
     </message>
     <message>
         <source>SCREENS</source>
-        <translation type="unfinished"></translation>
+        <translation>Ecrane</translation>
     </message>
     <message>
         <source>Change the resolution of your display and arrange your displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Schimbați rezoluția și aranjați monitoarele</translation>
     </message>
     <message>
         <source>Arrange and Resize Displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Aranjează și redimensionează monitoarele</translation>
     </message>
     <message>
         <source>Your physical location will be sent to MET Norway to determine sunrise and sunset times.</source>
-        <translation type="unfinished"></translation>
+        <translation>Locația dumneavoastră va fi trimisă la MET Norway pentru a determina ora răsăritului și a apusului.</translation>
     </message>
     <message>
         <source>SCALING</source>
-        <translation type="unfinished"></translation>
+        <translation>Scalare</translation>
     </message>
     <message>
         <source>If items on the screen are too small, you can scale them up to make them easier to see.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dacă lucrurile de pe ecran sunt prea mici, puteți să le măriți pentru a le face mai ușor de văzut.</translation>
     </message>
     <message>
         <source>100% (96 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>100% (96DPI)</translation>
     </message>
     <message>
         <source>125% (120 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>125% (120 DPI)</translation>
     </message>
     <message>
         <source>150% (144 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>150% (144 DPI)</translation>
     </message>
     <message>
         <source>200% (192 DPI)</source>
-        <translation type="unfinished"></translation>
+        <translation>200% (192 DPI)</translation>
     </message>
 </context>
 <context>
@@ -122,11 +122,11 @@
     <name>RedshiftDaemon</name>
     <message>
         <source>Redshift Active</source>
-        <translation type="unfinished"></translation>
+        <translation>Redshift activ</translation>
     </message>
     <message>
         <source>Disable Redshift until tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Dezactivează Redshift până mâine</translation>
     </message>
 </context>
 </TS>
