89b0afbe8693dca783be007c985b14838ba73956
Atahan Reis <atahanreis@icloud.com>
2020-12-21 11:50:55 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (269 of 269 strings)
    
    Translation: theDesk/theDesk Networking
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-networking/tr/

--- plugins/NetworkPlugin/translations/tr_TR.ts
+++ plugins/NetworkPlugin/translations/tr_TR.ts
@@ -599,6 +599,7 @@
         <source>You have %n remaining tries</source>
         <translation>
             <numerusform>%n deneme hakkınız kaldı</numerusform>
+            <numerusform>%n deneme hakkınız kaldı</numerusform>
         </translation>
     </message>
     <message>
@@ -717,6 +718,7 @@
     <message numerus="yes">
         <source>You have %n remaining tries</source>
         <translation>
+            <numerusform>%n deneme hakkınız kaldı</numerusform>
             <numerusform>%n deneme hakkınız kaldı</numerusform>
         </translation>
     </message>
