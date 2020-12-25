b95bf8faba7aa83d5f1dd33e9d9e453c4e199c02
Atahan Reis <atahanreis@icloud.com>
2020-12-25 17:32:46 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: theDesk/theDesk Notifications
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-notifications/en_GB/

--- plugins/NotificationsPlugin/translations/en_GB.ts
+++ plugins/NotificationsPlugin/translations/en_GB.ts
@@ -1,70 +1,70 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>MprisChunk</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Media Players</source>
-        <translation type="unfinished"></translation>
+        <translation>Media Players</translation>
     </message>
 </context>
 <context>
     <name>NotificationAppGroup</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Uncategorised</source>
-        <translation type="unfinished"></translation>
+        <translation>Uncategorised</translation>
     </message>
 </context>
 <context>
     <name>NotificationWidget</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
 </context>
 <context>
     <name>NotificationsDrawer</name>
     <message>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
 </context>
 <context>
     <name>NotificationsDrawerWidget</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
 </context>
 <context>
     <name>NotificationsStatusCenterPane</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Notifications</translation>
     </message>
     <message>
         <source>Is it Friday yet?</source>
-        <translation type="unfinished"></translation>
+        <translation>Is it Friday yet?</translation>
     </message>
     <message>
         <source>You&apos;ve caught up with your notifications!</source>
-        <translation type="unfinished"></translation>
+        <translation>You&apos;ve caught up with your notifications!</translation>
     </message>
     <message>
         <source>QUIET MODE</source>
-        <translation type="unfinished"></translation>
+        <translation>QUIET MODE</translation>
     </message>
 </context>
 </TS>
