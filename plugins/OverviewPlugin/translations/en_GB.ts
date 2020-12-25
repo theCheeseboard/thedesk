2555e5c2d8b780ec61884065493c2260254a2a4c
Atahan Reis <atahanreis@icloud.com>
2020-12-25 17:28:34 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: theDesk/theDesk Overview
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-overview/en_GB/

--- plugins/OverviewPlugin/translations/en_GB.ts
+++ plugins/OverviewPlugin/translations/en_GB.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>OverviewPane</name>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="106"/>
         <source>Hi %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hi %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="108"/>
         <source>Good morning, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Good morning, %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="110"/>
         <source>Good afternoon, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Good afternoon, %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="112"/>
         <source>Good evening, %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Good evening, %1!</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.cpp" line="254"/>
         <source>Overview</source>
-        <translation type="unfinished"></translation>
+        <translation>Overview</translation>
     </message>
     <message>
         <location filename="../OverviewPane/overviewpane.ui" line="102"/>
         <source>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</translation>
     </message>
 </context>
 </TS>
