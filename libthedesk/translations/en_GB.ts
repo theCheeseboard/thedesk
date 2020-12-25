7ba0aeb192bbd5c00e177d0e341577c7aa01dba7
Atahan Reis <atahanreis@icloud.com>
2020-12-25 19:12:12 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: theDesk/theDesk Core
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-core/en_GB/

--- libthedesk/translations/en_GB.ts
+++ libthedesk/translations/en_GB.ts
@@ -1,136 +1,136 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>ActionQuickWidget</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
 </context>
 <context>
     <name>LocaleSelector</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Select a locale</translation>
     </message>
 </context>
 <context>
     <name>OnboardingBetaThankYou</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Thank you!</source>
-        <translation type="unfinished"></translation>
+        <translation>Thank you!</translation>
     </message>
     <message>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Next</translation>
     </message>
     <message>
         <source>A Message</source>
-        <translation type="unfinished"></translation>
+        <translation>A Message</translation>
     </message>
 </context>
 <context>
     <name>OnboardingFinal</name>
     <message>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>That&apos;s everything!</translation>
     </message>
     <message>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>We hope you enjoy using theDesk!</translation>
     </message>
     <message>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Get Started</translation>
     </message>
     <message>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Done</translation>
     </message>
 </context>
 <context>
     <name>OnboardingVideo</name>
     <message>
         <source>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</translation>
     </message>
 </context>
 <context>
     <name>OnboardingWelcome</name>
     <message>
         <source>Welcome!</source>
-        <translation type="unfinished"></translation>
+        <translation>Welcome!</translation>
     </message>
     <message>
         <source>Let&apos;s get you up and running!</source>
-        <translation type="unfinished"></translation>
+        <translation>Let&apos;s get you up and running!</translation>
     </message>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Language</translation>
     </message>
     <message>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Emergency</translation>
     </message>
     <message>
         <source>Let&apos;s go!</source>
-        <translation type="unfinished"></translation>
+        <translation>Let&apos;s go!</translation>
     </message>
     <message>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>Welcome</translation>
     </message>
 </context>
 <context>
     <name>QuickWidgetContainer</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
 </context>
 <context>
     <name>QuietModeManager</name>
     <message>
         <source>Allows all sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>Allows all sounds</translation>
     </message>
     <message>
         <source>Hides non-critical notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Hides non-critical notifications</translation>
     </message>
     <message>
         <source>Hides all notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Hides all notifications</translation>
     </message>
     <message>
         <source>Mutes all sound altogether</source>
-        <translation type="unfinished"></translation>
+        <translation>Mutes all sound altogether</translation>
     </message>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Sound</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>Critical Only</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>No Notifications</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Mute</translation>
     </message>
 </context>
 </TS>
