d97d41d77b7865b64e996c183f19292f506b8b13
Atahan Reis <atahanreis@icloud.com>
2020-12-25 19:33:29 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/en_GB/

--- plugins/ThemePlugin/translations/en_GB.ts
+++ plugins/ThemePlugin/translations/en_GB.ts
@@ -1,43 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>OnboardingTheme</name>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="91"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Theme</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Select a theme. You&apos;ll be able to change this at any time.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Dark</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="108"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Light</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="238"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Next</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="76"/>
         <source>Intellect has spoken; blinding shall be no more!</source>
-        <translation type="unfinished"></translation>
+        <translation>Intellect has spoken; blinding shall be no more!</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="81"/>
         <source>One whose wisdom has brought them the light of day!</source>
-        <translation type="unfinished"></translation>
+        <translation>One whose wisdom has brought them the light of day!</translation>
     </message>
 </context>
 <context>
@@ -46,132 +46,132 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="198"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Theme</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Dark</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Light</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Change the colours of the interface</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>COLOURS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="224"/>
         <source>WINDOW BORDERS</source>
-        <translation type="unfinished"></translation>
+        <translation>WINDOW BORDERS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="231"/>
         <source>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="243"/>
         <source>Set Window Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Set Window Borders</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="274"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Fixed Width Font</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="281"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Interface Font</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="304"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>FONTS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="370"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Widget Theme</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="434"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Button</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="447"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Radio Button 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="457"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Using the theme set above, interface elements will look like this:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="464"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Radio Button 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="471"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Check Box 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="487"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>DEMO</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="494"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Check Box 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="508"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Type here!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="543"/>
         <source>Transparency Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Transparency Effects</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="563"/>
         <source>EFFECTS</source>
-        <translation type="unfinished"></translation>
+        <translation>EFFECTS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="363"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Set the appearance of buttons, text boxes and other interface elements</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="383"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>WIDGETS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Base Colour</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Accent Colour</translation>
     </message>
 </context>
 </TS>
