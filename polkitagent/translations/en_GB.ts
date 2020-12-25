eec159d392b38f1ab376c7ae4d5bc500a13860e2
Atahan Reis <atahanreis@icloud.com>
2020-12-25 17:29:35 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: theDesk/theDesk Polkit
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-polkit/en_GB/

--- polkitagent/translations/en_GB.ts
+++ polkitagent/translations/en_GB.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>AuthWindow</name>
     <message>
         <location filename="../authwindow.ui" line="35"/>
         <source>PolicyKit Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>PolicyKit Authentication</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="199"/>
         <source>Enter your password to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter your password to confirm this action</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="441"/>
         <source>Pass the authentication challenge to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Pass the authentication challenge to confirm this action</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="160"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="167"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Message</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../identitypopover.ui" line="35"/>
         <source>Identity</source>
-        <translation type="unfinished"></translation>
+        <translation>Identity</translation>
     </message>
     <message>
         <location filename="../identitypopover.ui" line="42"/>
         <source>Who do you want to authenticate as?</source>
-        <translation type="unfinished"></translation>
+        <translation>Who do you want to authenticate as?</translation>
     </message>
 </context>
 </TS>
