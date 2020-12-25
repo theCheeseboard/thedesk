6bc0d1b0ec87768b3fcfff9a1d36eed83651bed2
Atahan Reis <atahanreis@icloud.com>
2020-12-25 17:30:08 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: theDesk/theDesk Screenshots
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-screenshots/en_GB/

--- plugins/ScreenshotPlugin/translations/en_GB.ts
+++ plugins/ScreenshotPlugin/translations/en_GB.ts
@@ -1,48 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>PenButton</name>
     <message>
         <source>Erase-o</source>
         <comment>This and the next translation span two lines. Be spiffy with these!</comment>
-        <translation type="unfinished"></translation>
+        <translation>Erase-o</translation>
     </message>
     <message>
         <source>matic 2000</source>
         <comment>This and the previous translation span two lines. Be spiffy with these!</comment>
-        <translation type="unfinished"></translation>
+        <translation>matic 2000</translation>
     </message>
 </context>
 <context>
     <name>ScreenshotWindow</name>
     <message>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <source>Discard</source>
-        <translation type="unfinished"></translation>
+        <translation>Discard</translation>
     </message>
     <message>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy</translation>
     </message>
     <message>
         <source>SCREENSHOT</source>
-        <translation type="unfinished"></translation>
+        <translation>SCREENSHOT</translation>
     </message>
     <message>
         <source>Crop</source>
-        <translation type="unfinished"></translation>
+        <translation>Crop</translation>
     </message>
     <message>
         <source>Redact</source>
-        <translation type="unfinished"></translation>
+        <translation>Redact</translation>
     </message>
     <message>
         <source>Reset Markup</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset Markup</translation>
     </message>
 </context>
 </TS>
