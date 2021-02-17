78a04c5fc1ac2ef43b9d9040290f02b4c883f98d
Victor Tran <vicr12345@gmail.com>
2021-02-17 12:55:36 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (55 of 55 strings)
    
    Translation: theDesk/theDesk Bluetooth
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-bluetooth/vi/

--- plugins/BluetoothPlugin/translations/vi.ts
+++ plugins/BluetoothPlugin/translations/vi.ts
@@ -6,8 +6,8 @@
     <message numerus="yes">
         <location filename="../chunk/bluetoothchunk.cpp" line="60"/>
         <source>%n devices connected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n thiết bị kết nối</numerusform>
         </translation>
     </message>
 </context>
@@ -18,62 +18,62 @@
         <location filename="../settings/bluetoothsettingspane.cpp" line="76"/>
         <location filename="../settings/bluetoothsettingspane.cpp" line="181"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="106"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HOẠT ĐỘNG</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="113"/>
         <source>Pair New Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép thiết bị mới</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="139"/>
         <source>Visible to other devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Phát hiện trên thiết bị khác</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="166"/>
         <source>DISCOVERABILITY</source>
-        <translation type="unfinished"></translation>
+        <translation>HIỂN THỊ TRÊN THIẾT BỊ KHÁC</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="213"/>
         <source>DEVICES</source>
-        <translation type="unfinished"></translation>
+        <translation>THIẾT BỊ</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="272"/>
         <source>Bluetooth is unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth không khả dụng</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="295"/>
         <source>Install a Bluetooth card to use Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Hãy cài một bộ mạch Bluetooth để sử dụng Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="340"/>
         <source>Bluetooth is disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth bị vô hiệu</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="363"/>
         <source>Switch Bluetooth on to connect to Bluetooth devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Bật Bluetooth để kết nối với thiết bị Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="373"/>
         <source>Enable Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bật Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.cpp" line="119"/>
         <source>To pair with this device, look for %1 on the other device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Để ghép với thiết bị này, tìm tên %1 trên thiết bị kia.</translation>
     </message>
 </context>
 <context>
@@ -82,44 +82,44 @@
         <location filename="../btagent.cpp" line="89"/>
         <location filename="../btagent.cpp" line="119"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="90"/>
         <location filename="../btagent.cpp" line="120"/>
         <source>Pair with %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép với %1?</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="91"/>
         <source>In order to complete pairing, confirm that the following pairing code is shown on %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Để hoàn thành hoạt động ghép nối, vui lòng đảm bảo rằng mã dưới đây hiển thị trên %1.</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="92"/>
         <source>If it is, you can accept the pairing request.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu mã có hiển thị, bạn có thể chấp nhận yêu cầu ghép nối.</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="93"/>
         <location filename="../btagent.cpp" line="122"/>
         <source>Decline</source>
-        <translation type="unfinished"></translation>
+        <translation>Từ chối</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="94"/>
         <source>Accept and Pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Chấp nhận và Ghép nối</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="121"/>
         <source>Accept the pairing request from %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Chấp nhận yêu cầu ghép nối từ %1?</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="123"/>
         <source>Pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép nối</translation>
     </message>
 </context>
 <context>
@@ -127,97 +127,97 @@
     <message>
         <location filename="../common.cpp" line="29"/>
         <source>Phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Điện thoại</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="31"/>
         <source>Modem</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ điều giải</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="33"/>
         <source>Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy tính</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="35"/>
         <source>Networking</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị mạng</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="37"/>
         <source>Headset</source>
-        <translation type="unfinished"></translation>
+        <translation>Tai nghe có micrô</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="39"/>
         <source>Headphones</source>
-        <translation type="unfinished"></translation>
+        <translation>Tai nghe</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="41"/>
         <source>Audio/Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Âm thanh/Video</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="43"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Bàn phím</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="45"/>
         <source>Mouse</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuột</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="47"/>
         <source>Gamepad</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ điều khiển</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="49"/>
         <source>Drawing Tablet</source>
-        <translation type="unfinished"></translation>
+        <translation>Bảng vẽ</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="51"/>
         <source>Peripheral</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngoại vi</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="53"/>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy ảnh</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="55"/>
         <source>Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy in</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="57"/>
         <source>Imaging</source>
-        <translation type="unfinished"></translation>
+        <translation>Máy hình ảnh</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="59"/>
         <source>Wearable</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị đeo</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="61"/>
         <source>Toy</source>
-        <translation type="unfinished"></translation>
+        <translation>Đồ chơi</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="63"/>
         <source>Health</source>
-        <translation type="unfinished"></translation>
+        <translation>Thiết bị sức khỏe</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="65"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Không xác định</translation>
     </message>
 </context>
 <context>
@@ -225,32 +225,32 @@
     <message>
         <location filename="../settings/devicepopover.ui" line="124"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HOẠT ĐỘNG</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="133"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Kết nối</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="140"/>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngắt kết nối</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="147"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.cpp" line="72"/>
         <source>Failed to connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể kết nối</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.cpp" line="75"/>
         <source>Connecting to %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể kết nối với %1.</translation>
     </message>
 </context>
 <context>
@@ -258,47 +258,47 @@
     <message>
         <location filename="../settings/pairpopover.ui" line="57"/>
         <source>Pair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghép thiết bị mới</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.ui" line="64"/>
         <source>To get started, ensure the device you want to pair is visible. This may be achieved by opening the Bluetooth settings or pressing a button on the other device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Để bắt đầu, hãy đảm bảo thiết bị bạn muốn ghép nối cho sẽ phát hiện với thiết bị khác. Điều này có thể đạt được bằng cách cài đặt Bluetooth hoặc nhấn một nút trên thiết bị kia.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.ui" line="383"/>
         <source>Confirm Pairing Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Xác nhận mã ghép</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="92"/>
         <source>Then press ENTER or RETURN.</source>
-        <translation type="unfinished"></translation>
+        <translation>Rồi bấm ENTER hoặc RETURN.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="130"/>
         <source>Failed to pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghép nối</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="133"/>
         <source>Pairing with %1 failed because the pairing code was incorrect.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghép với %1 tại vì mã ghép không đúng.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="136"/>
         <source>Pairing with %1 failed because the device rejected the pairing request.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghép với %1 tại vì thiết bị kia từ chối yêu cầu ghép nối.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="139"/>
         <source>Pairing with %1 failed because the pairing code was not confirmed in time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghép với %1 tại vì mất quá nhiều thời gian xác nhận mã ghép.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="142"/>
         <source>Pairing with %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghép với %1.</translation>
     </message>
 </context>
 </TS>
