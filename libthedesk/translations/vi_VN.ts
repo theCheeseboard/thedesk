db3a8cf1ef7b1a121110616edd0250c9a89e2440
Victor Tran <vicr12345@gmail.com>
2021-02-18 04:57:02 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 81.4% (22 of 27 strings)
    
    Translation: theDesk/theDesk Core
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-core/vi/

--- libthedesk/translations/vi_VN.ts
+++ libthedesk/translations/vi_VN.ts
@@ -12,34 +12,34 @@
     <name>OnboardingBetaThankYou</name>
     <message>
         <source>Thank you!</source>
-        <translation type="unfinished"></translation>
+        <translation>Cảm ơn!</translation>
     </message>
     <message>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp</translation>
     </message>
     <message>
         <source>A Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Lá thư cho bạn</translation>
     </message>
 </context>
 <context>
     <name>OnboardingFinal</name>
     <message>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>Đó là tất cả!</translation>
     </message>
     <message>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi hy vọng bạn thích sử dụng theDesk!</translation>
     </message>
     <message>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Bắt đầu</translation>
     </message>
     <message>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoàn thành</translation>
     </message>
 </context>
 <context>
@@ -53,62 +53,62 @@
     <name>OnboardingWelcome</name>
     <message>
         <source>Welcome!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chào mừng!</translation>
     </message>
     <message>
         <source>Let&apos;s get you up and running!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi sẽ đi theo với bạn để thiết lập thiết bị này!</translation>
     </message>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngôn ngữ</translation>
     </message>
     <message>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>Khẩn cấp</translation>
     </message>
     <message>
         <source>Let&apos;s go!</source>
-        <translation type="unfinished"></translation>
+        <translation>Bắt đầu!</translation>
     </message>
     <message>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>Chào mừng</translation>
     </message>
 </context>
 <context>
     <name>QuietModeManager</name>
     <message>
         <source>Allows all sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>Phát các âm thanh</translation>
     </message>
     <message>
         <source>Hides non-critical notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Ẩn thông báo không quan trọng</translation>
     </message>
     <message>
         <source>Hides all notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Ẩn các thông báo</translation>
     </message>
     <message>
         <source>Mutes all sound altogether</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt âm hoàn toàn</translation>
     </message>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>Âm thanh</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>Chỉ quan trọng</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Không có thông báo</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>Im lặng</translation>
     </message>
 </context>
 </TS>
