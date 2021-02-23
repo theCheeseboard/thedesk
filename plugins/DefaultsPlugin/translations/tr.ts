79bf053adf02262fae5c6303c2362c8a16055d2f
Aleyna Tilki <atahanreis@icloud.com>
2021-02-23 11:31:43 +0000

    Added translation using Weblate (Turkish)

---
+++ plugins/DefaultsPlugin/translations/tr.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="tr">
+<context>
+    <name>DefaultsPane</name>
+    <message>
+        <location filename="../defaultspane.ui" line="35"/>
+        <location filename="../defaultspane.cpp" line="81"/>
+        <source>Default Applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="54"/>
+        <source>Web Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="61"/>
+        <source>Set your default apps below</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="68"/>
+        <source>Mail Client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
