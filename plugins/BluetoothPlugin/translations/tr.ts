765fec328ab5d4365d3e7ae6b90d20ed067b6d7d
Aleyna Tilki <atahanreis@icloud.com>
2021-03-03 09:02:20 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: theDesk/theDesk Bluetooth
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-bluetooth/tr/

--- plugins/BluetoothPlugin/translations/tr.ts
+++ plugins/BluetoothPlugin/translations/tr.ts
@@ -129,47 +129,47 @@
         <location filename="../btobex.cpp" line="111"/>
         <location filename="../btobex.cpp" line="118"/>
         <source>Bluetooth</source>
-        <translation type="unfinished">Bluetooth</translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="72"/>
         <source>Incoming file</source>
-        <translation type="unfinished"></translation>
+        <translation>Gelen dosya</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="73"/>
         <source>%1 is sending you the file %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 seni %2 dosyasını gönderiyor.</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="74"/>
         <source>Decline</source>
-        <translation type="unfinished">Reddet</translation>
+        <translation>Reddet</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="75"/>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>Kabul et</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="112"/>
         <source>File received</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya alındı</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="113"/>
         <source>The file %1 has been received and can be found in your Downloads folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 dosyası alındı ve indirilenler klasörünüzde bulunabilir.</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="119"/>
         <source>File transfer failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya aktarımı başarısız oldu</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="120"/>
         <source>The file %1 was unable to be received.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 dosyası alınamadı.</translation>
     </message>
 </context>
 <context>
