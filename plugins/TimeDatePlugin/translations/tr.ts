53db91cbd4159f4b53fa3d1a29cf283e96f4523b
Aleyna Tilki <atahanreis@icloud.com>
2021-03-09 02:33:33 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: theDesk/theDesk Time and Date
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-time-and-date/tr/

--- plugins/TimeDatePlugin/translations/tr.ts
+++ plugins/TimeDatePlugin/translations/tr.ts
@@ -7,32 +7,32 @@
         <location filename="../settings/datetimepane.ui" line="35"/>
         <location filename="../settings/datetimepane.cpp" line="71"/>
         <source>Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarih ve Saat</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="84"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HAREKETLER</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="111"/>
         <source>Change Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarih ve saati değiştir</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="118"/>
         <source>Change Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Saat Dilimini Değiştir</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="146"/>
         <source>Synchronise clock with the Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Saati İnternet ile senkronize etmeye başlayın</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="166"/>
         <source>TIME SYNCHRONISATION</source>
-        <translation type="unfinished"></translation>
+        <translation>ZAMAN SENKRONİZASYONU</translation>
     </message>
 </context>
 <context>
@@ -41,27 +41,27 @@
         <location filename="../onboarding/onboardingtimezone.ui" line="35"/>
         <location filename="../onboarding/onboardingtimezone.cpp" line="53"/>
         <source>Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Saat Dilimi</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to display the current time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nerede bulunuyorsun? Bu bilgileri geçerli saati görüntülemek için kullanacağız.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="71"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Ara</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="125"/>
         <source>Use the Internet to synchronise the system clock (recommended)</source>
-        <translation type="unfinished"></translation>
+        <translation>Sistem saatini senkronize etmek için interneti kullanın (her zaman önerilir)</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="177"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonraki</translation>
     </message>
 </context>
 <context>
@@ -69,17 +69,17 @@
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="57"/>
         <source>Set Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Saat Dilimini Ayarla</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="64"/>
         <source>Where are you now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Şu anda neredesin?</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="90"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Ara</translation>
     </message>
 </context>
 </TS>
