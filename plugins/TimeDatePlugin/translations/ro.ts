38cff799b6a8fb62f26e42d2751de48dacbbfe98
thonkdifferent <thonkdifferent@outlook.com>
2021-05-20 21:41:23 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: theDesk/theDesk Time and Date
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-time-and-date/ro/

--- plugins/TimeDatePlugin/translations/ro.ts
+++ plugins/TimeDatePlugin/translations/ro.ts
@@ -7,32 +7,32 @@
         <location filename="../settings/datetimepane.ui" line="35"/>
         <location filename="../settings/datetimepane.cpp" line="71"/>
         <source>Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Ora și data</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="84"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>ACȚIUNI</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="111"/>
         <source>Change Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Schimbați ora și data</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="118"/>
         <source>Change Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Schimbați fusul orar</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="146"/>
         <source>Synchronise clock with the Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Sincronizați ceasul cu internetul</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="166"/>
         <source>TIME SYNCHRONISATION</source>
-        <translation type="unfinished"></translation>
+        <translation>SINCRONIZAREA CEASULUI</translation>
     </message>
 </context>
 <context>
@@ -41,27 +41,27 @@
         <location filename="../onboarding/onboardingtimezone.ui" line="35"/>
         <location filename="../onboarding/onboardingtimezone.cpp" line="53"/>
         <source>Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Fus orar</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to display the current time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Unde vă aflați? Vom folosi informația aceasta pentru a vă arăta ora curentă.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="71"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Căutare</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="125"/>
         <source>Use the Internet to synchronise the system clock (recommended)</source>
-        <translation type="unfinished"></translation>
+        <translation>Folosiți internetul pentru a sincroniza ceasul sistemului (recomandat)</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="177"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Următorul</translation>
     </message>
 </context>
 <context>
@@ -69,17 +69,17 @@
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="57"/>
         <source>Set Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurați fusul orar</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="64"/>
         <source>Where are you now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Unde vă situați acum?</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="90"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Căutare</translation>
     </message>
 </context>
 </TS>
