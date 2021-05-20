ece7193d212e08a9392a86c48efa0d8e6fdb62d8
thonkdifferent <thonkdifferent@outlook.com>
2021-05-20 21:28:40 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/ro/

--- plugins/ThemePlugin/translations/ro.ts
+++ plugins/ThemePlugin/translations/ro.ts
@@ -7,37 +7,37 @@
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="91"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Temă</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Selectați o temă. O veți putea schimba oricând.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Întunecat</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="108"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Luminos</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="238"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Următorul</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="76"/>
         <source>Intellect has spoken; blinding shall be no more!</source>
-        <translation type="unfinished"></translation>
+        <translation>Intelectul a vorbit; lumina orbitoare să dispară!</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="81"/>
         <source>One whose wisdom has brought them the light of day!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pentru cei pe care înțelepciunea le-a adus lumina zilei!</translation>
     </message>
 </context>
 <context>
@@ -46,132 +46,132 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="198"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Temă</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Culoare de accent</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Întunecat</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Luminos</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Schimbați culorile interfeței</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Culoarea de bază</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>CULORI</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="224"/>
         <source>WINDOW BORDERS</source>
-        <translation type="unfinished"></translation>
+        <translation>CONTURUL FERESTRELOR</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="231"/>
         <source>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dacă nu arată corect conturul ferestrelor, putem rezolva acest lucru pentru dvs. Acest proces va schimba conturul ferestrelor în KDE cât și în orice alt desktop care folosește KWin pentru gestiunea ferestrelor.</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="243"/>
         <source>Set Window Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurați conturul ferestrelor</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="274"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font de lățime fixă</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="281"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font interfață</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="304"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>FONTURI</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="363"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Selectați aspectul butoanelor, căsuțelor de text și a altor elemente de interfață</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="370"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Temă widgeturi</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="383"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>WIDGETURI</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="434"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Buton</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="447"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Buton radio 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="457"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Folosind tema setată mai sus, elementele de interfață vor arăta așa:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="464"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Buton radio 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="471"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Casetă de selectare 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="487"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>DEMO</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="494"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Casetă de selectare 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="508"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastați aici!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="543"/>
         <source>Transparency Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Efecte de transparență</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="563"/>
         <source>EFFECTS</source>
-        <translation type="unfinished"></translation>
+        <translation>EFECTE</translation>
     </message>
 </context>
 </TS>
