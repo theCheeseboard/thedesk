65a8f3c815ac8c1cb4cd31261245b55fb334f505
thonkdifferent <thonkdifferent@outlook.com>
2021-05-20 21:36:04 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: theDesk/theDesk Accessibility
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-accessibility/ro/

--- plugins/AccessibilityPlugin/translations/ro.ts
+++ plugins/AccessibilityPlugin/translations/ro.ts
@@ -7,37 +7,37 @@
         <location filename="../settings/accessibilitysettingspane.ui" line="35"/>
         <location filename="../settings/accessibilitysettingspane.cpp" line="66"/>
         <source>Accessibility</source>
-        <translation type="unfinished"></translation>
+        <translation>Accesibilitate</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="84"/>
         <source>Sticky Keys</source>
-        <translation type="unfinished"></translation>
+        <translation>Taste persistente</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="104"/>
         <source>TYPING</source>
-        <translation type="unfinished"></translation>
+        <translation>TASTARE</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="114"/>
         <source>Allows you to press modifier keys sequentially</source>
-        <translation type="unfinished"></translation>
+        <translation>Vă permite să apăsați secvențial tastele modificator</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="149"/>
         <source>POINTING</source>
-        <translation type="unfinished"></translation>
+        <translation>INDICARE</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="163"/>
         <source>Mouse Keys</source>
-        <translation type="unfinished"></translation>
+        <translation>Mouse din tastatură</translation>
     </message>
     <message>
         <location filename="../settings/accessibilitysettingspane.ui" line="173"/>
         <source>Allows you to move the cursor using the number pad</source>
-        <translation type="unfinished"></translation>
+        <translation>Vă permite să mutați cursorul cu ajutorul tastaturii numerice</translation>
     </message>
 </context>
 <context>
@@ -45,7 +45,7 @@
     <message>
         <location filename="../chunks/stickykeyschunk.cpp" line="57"/>
         <source>Disable Sticky Keys</source>
-        <translation type="unfinished"></translation>
+        <translation>Dezactivați tastele persistente</translation>
     </message>
 </context>
 </TS>
