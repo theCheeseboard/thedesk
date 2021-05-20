9cc75f564e6a535b0008182466709636a6013356
thonkdifferent <thonkdifferent@outlook.com>
2021-05-20 19:59:24 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: theDesk/theDesk Overview
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-overview/ro/

--- plugins/OverviewPlugin/translations/ro_RO.ts
+++ plugins/OverviewPlugin/translations/ro_RO.ts
@@ -26,19 +26,19 @@
     <message numerus="yes">
         <location filename="../OverviewPane/overviewpane.cpp" line="208"/>
         <source>Daylight Savings Time is ending on %2. The clock will automatically shift backwards by %n hour(s).</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Ora de vară se va încheia pe data de %2. Ceasul va fi dat cu o oră în urmă.</numerusform>
+            <numerusform>Ora de vară se va încheia pe data de %2. Ceasul va fi dat cu %n ore în urmă.</numerusform>
+            <numerusform>Ora de vară se va încheia pe data de %2. Ceasul va fi dat cu %n ore în urmă.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../OverviewPane/overviewpane.cpp" line="210"/>
         <source>Daylight Savings Time is starting on %2. The clock will automatically shift forwards by %n hour(s).</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Ora de vară va începe pe data de %2. Ceasul va fi dat cu o oră în față.</numerusform>
+            <numerusform>Ora de vară va începe pe data de %2. Ceasul va fi dat cu %n ore în față.</numerusform>
+            <numerusform>Ora de vară va începe pe data de %2. Ceasul va fi dat cu %n ore în față.</numerusform>
         </translation>
     </message>
     <message>
@@ -49,7 +49,7 @@
     <message>
         <location filename="../OverviewPane/overviewpane.ui" line="102"/>
         <source>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</source>
-        <translation>Sperăm că aveți o zi minunată. Uitați ce avem pentru dumneavoastră:</translation>
+        <translation>Sperăm că vă simțiți minunat. Uitați ce avem pentru dumneavoastră:</translation>
     </message>
 </context>
 </TS>
