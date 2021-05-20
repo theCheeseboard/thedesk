b8f7c0466179301794d78e696572baa8151fd3dd
thonkdifferent <thonkdifferent@outlook.com>
2021-05-20 21:38:43 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: theDesk/theDesk Input Devices
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-input-devices/ro/

--- plugins/InputPlugin/translations/ro.ts
+++ plugins/InputPlugin/translations/ro.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../settings/inputsettingsleftpane.ui" line="35"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Intrare</translation>
     </message>
     <message>
         <location filename="../settings/inputsettingsleftpane.ui" line="46"/>
         <source>Mouse and Touchpad</source>
-        <translation type="unfinished"></translation>
+        <translation>Mouse și tastatură</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../settings/inputsettingspane.cpp" line="55"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Intrare</translation>
     </message>
 </context>
 <context>
@@ -27,57 +27,57 @@
     <message>
         <location filename="../settings/mousesettings.ui" line="35"/>
         <source>Mouse and Touchpad</source>
-        <translation type="unfinished"></translation>
+        <translation>Mouse și tastatură</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="106"/>
         <source>CLICKING</source>
-        <translation type="unfinished"></translation>
+        <translation>APĂSARE</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="118"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Stânga</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="131"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Dreapta</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="152"/>
         <source>Primary Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Butonul principal</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="159"/>
         <source>Tap to Click</source>
-        <translation type="unfinished"></translation>
+        <translation>Atingeți pentru click</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="205"/>
         <source>Natural Scrolling</source>
-        <translation type="unfinished"></translation>
+        <translation>Derulare naturală</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="218"/>
         <source>SCROLLING</source>
-        <translation type="unfinished"></translation>
+        <translation>DERULARE</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="228"/>
         <source>Reverse the direction that you scroll in so that the content moves in the same direction as your fingers.</source>
-        <translation type="unfinished"></translation>
+        <translation>Inversează direcția în care derulați astfel încât conținutul se mișcă în aceași direcție ca degetele.</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="280"/>
         <source>Settings Unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Setări indisponibile</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="290"/>
         <source>Mouse settings can&apos;t be changed on this platform.</source>
-        <translation type="unfinished"></translation>
+        <translation>Setările mouse-ului nu pot fi schimbate pe această platformă.</translation>
     </message>
 </context>
 </TS>
