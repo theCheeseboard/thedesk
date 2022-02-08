aa6db28fba84f485d33098e382d1ba6fbc12923e
Victor Tran <vicr12345@gmail.com>
2022-02-08 14:12:16 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (42 of 42 strings)
    
    Translation: theDesk/theDesk Power
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-power/vi/

--- plugins/PowerPlugin/translations/vi.ts
+++ plugins/PowerPlugin/translations/vi.ts
@@ -6,18 +6,18 @@
     <message>
         <location filename="../eventhandler.cpp" line="87"/>
         <source>Unable to inhibit logind power management</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ngăn cản việc quản lý điện logind</translation>
     </message>
     <message>
         <location filename="../eventhandler.cpp" line="121"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạm dừng</translation>
     </message>
     <message numerus="yes">
         <location filename="../eventhandler.cpp" line="122"/>
         <source>%n seconds</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n giây</numerusform>
         </translation>
     </message>
     <message>
@@ -26,22 +26,22 @@
         <location filename="../eventhandler.cpp" line="217"/>
         <location filename="../eventhandler.cpp" line="227"/>
         <source>Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Pin</translation>
     </message>
     <message>
         <location filename="../eventhandler.cpp" line="208"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang sạc</translation>
     </message>
     <message>
         <location filename="../eventhandler.cpp" line="218"/>
         <source>Full</source>
-        <translation type="unfinished"></translation>
+        <translation>Đã đầy</translation>
     </message>
     <message>
         <location filename="../eventhandler.cpp" line="228"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang xả</translation>
     </message>
 </context>
 <context>
@@ -50,12 +50,12 @@
         <location filename="../plugin.cpp" line="93"/>
         <location filename="../plugin.cpp" line="105"/>
         <source>Power Stretch</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiết kiệm pin</translation>
     </message>
     <message>
         <location filename="../plugin.cpp" line="109"/>
         <source>Disable Power Stretch</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt chế độ tiết kiệm pin</translation>
     </message>
 </context>
 <context>
@@ -64,171 +64,171 @@
         <location filename="../settings/powersettings.ui" line="35"/>
         <location filename="../settings/powersettings.cpp" line="77"/>
         <source>Power</source>
-        <translation type="unfinished"></translation>
+        <translation>Điện</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="83"/>
         <source>POWER PROFILE</source>
-        <translation type="unfinished"></translation>
+        <translation>HỒ SƠ ĐIỆN</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="126"/>
         <source>Balanced</source>
-        <translation type="unfinished"></translation>
+        <translation>Cân bằng</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="103"/>
         <source>Power Stretch</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiết kiệm pin</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="116"/>
         <source>Reduce the performance of your device to save on power</source>
-        <translation type="unfinished"></translation>
+        <translation>Giảm hiệu suất của thiết bị để tiết kiệm năng lượng</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="149"/>
         <source>Performance</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiệu suất</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="90"/>
         <source>A power profile tweaks the way your system runs in order to prioritise performance or reduce power consumption.</source>
-        <translation type="unfinished"></translation>
+        <translation>Một hồ sơ điện điều chỉnh cách hệ thống của bạn chạy để ưu tiên hiệu suất hoặc giảm mức tiêu năng lượng.</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="139"/>
         <source>Performance and power consumption will be average</source>
-        <translation type="unfinished"></translation>
+        <translation>Cân bằng hiệu suất và mục tiêu năng lượng</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="162"/>
         <source>Performance will be increased at the cost of power consumption</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiệu suất cũng như mục tiêu năng lượng sẽ được tăng lên</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="196"/>
         <source>TIMEOUTS</source>
-        <translation type="unfinished"></translation>
+        <translation>THỜI GIAN CHỜ</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="209"/>
         <source>Switch off the screen after</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt màn hình sau</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="226"/>
         <source>If you leave your device idle for a period of time, we can automatically turn off the screen or suspend it for you.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nếu bạn để thiết bị chờ trong một khoảng thời gian, chúng tôi có thể tự động tắt màn hình hoặc tạm dừng nó cho bạn.</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="237"/>
         <location filename="../settings/powersettings.ui" line="278"/>
         <source>seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>giây</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="242"/>
         <location filename="../settings/powersettings.ui" line="283"/>
         <source>minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>phút</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="247"/>
         <location filename="../settings/powersettings.ui" line="288"/>
         <source>hours</source>
-        <translation type="unfinished"></translation>
+        <translation>giờ</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="252"/>
         <location filename="../settings/powersettings.ui" line="293"/>
         <source>never</source>
-        <translation type="unfinished"></translation>
+        <translation>không bao giờ</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="260"/>
         <source>Suspend after</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạm dừng sau</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="326"/>
         <source>When pressing the Power button</source>
-        <translation type="unfinished"></translation>
+        <translation>Khi nút nguồn bị bấm</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="346"/>
         <source>Ask</source>
-        <translation type="unfinished"></translation>
+        <translation>Hỏi tôi</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="351"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Tắt nguồn</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="356"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Khởi động lại</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="361"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạm dừng</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="366"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngủ đông</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="371"/>
         <source>Do Nothing</source>
-        <translation type="unfinished"></translation>
+        <translation>Không làm gì</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="379"/>
         <source>After suspending, lock the screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Khóa màn hình sau khi thiết bị bị tạm dừng</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.ui" line="338"/>
         <source>BUTTONS</source>
-        <translation type="unfinished"></translation>
+        <translation>NÚT</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="140"/>
         <source>Performance mode unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ hiệu suất không có sẵn</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="141"/>
         <source>unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>không sẵn</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="146"/>
         <source>Performance mode temporarily unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ hiệu suất không sẵn tạm thời</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="147"/>
         <source>temporarily unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>không sẵn tạm thời</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="158"/>
         <source>Performance mode is %1 because the device is on your lap.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ hiệu suất tại vì thiết bị này đang nằm trên đùi của bạn.</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="159"/>
         <source>Performance mode is %1 because your device is getting warm.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ hiệu suất tại vì thiết bị của bạn đang nóng lên.</translation>
     </message>
     <message>
         <location filename="../settings/powersettings.cpp" line="160"/>
         <source>Performance mode is %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chế độ hiệu suất %1.</translation>
     </message>
 </context>
 </TS>
