5d5f295ace7fcde235c0398700966636924fe27e
Victor Tran <vicr12345@gmail.com>
2022-02-08 13:18:41 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: theDesk/theDesk Time and Date
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-time-and-date/vi/

--- plugins/TimeDatePlugin/translations/vi.ts
+++ plugins/TimeDatePlugin/translations/vi.ts
@@ -7,32 +7,32 @@
         <location filename="../settings/datetimepane.ui" line="35"/>
         <location filename="../settings/datetimepane.cpp" line="71"/>
         <source>Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngày và giờ</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="84"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>HOẠT ĐỘNG</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="111"/>
         <source>Change Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Thay đổi ngày và giờ</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="118"/>
         <source>Change Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Thay đổi múi giờ</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="146"/>
         <source>Synchronise clock with the Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Đồng bộ đồng hồ với Internet</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="166"/>
         <source>TIME SYNCHRONISATION</source>
-        <translation type="unfinished"></translation>
+        <translation>ĐỒNG BỘ ĐỒNG HỒ</translation>
     </message>
 </context>
 <context>
@@ -41,27 +41,27 @@
         <location filename="../onboarding/onboardingtimezone.ui" line="35"/>
         <location filename="../onboarding/onboardingtimezone.cpp" line="53"/>
         <source>Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Múi giờ</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to display the current time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn đang ở đâu? Chúng tôi sẽ sử dụng thông tin này để đặt ngày và giờ.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="71"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="125"/>
         <source>Use the Internet to synchronise the system clock (recommended)</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng Internet để đồng bộ ngày và giờ (Đề xuất)</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="177"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiếp</translation>
     </message>
 </context>
 <context>
@@ -69,17 +69,17 @@
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="57"/>
         <source>Set Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt Múi Giờ</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="64"/>
         <source>Where are you now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn đang ở đâu bây giờ?</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="90"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Tìm kiếm</translation>
     </message>
 </context>
 </TS>
