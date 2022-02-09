c92d753e157b3a200706923e05d9a261aaef4e58
Weblate <noreply@weblate.org>
2022-02-09 12:09:19 +0000

    Added translation using Weblate (German)

---
+++ startdesk/translations/de.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="de">
+<context>
+    <name>BacktraceDialog</name>
+    <message>
+        <location filename="../crash/backtracedialog.ui" line="14"/>
+        <location filename="../crash/backtracedialog.ui" line="35"/>
+        <source>Error Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BacktracePopover</name>
+    <message>
+        <location filename="../crash/backtracepopover.ui" line="35"/>
+        <source>Error Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BacktraceWidget</name>
+    <message>
+        <location filename="../crash/backtracewidget.ui" line="50"/>
+        <source>Review the information below and ensure it does not contain any personal data. Attach the information to a bug report and we&apos;ll get back to you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/backtracewidget.ui" line="66"/>
+        <source>ACTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/backtracewidget.ui" line="73"/>
+        <source>Save to file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CrashWidget</name>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="38"/>
+        <source>Oh, Bonkers!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="61"/>
+        <location filename="../crash/crashwidget.cpp" line="36"/>
+        <source>theDesk had a problem and has stopped working.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="86"/>
+        <source>Relaunch theDesk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="97"/>
+        <source>Log Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.ui" line="151"/>
+        <source>View Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../crash/crashwidget.cpp" line="39"/>
+        <source>theDesk couldn&apos;t start because of a problem.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SplashController</name>
+    <message>
+        <location filename="../splash/splashcontroller.cpp" line="105"/>
+        <source>Oh, Bonkers!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../splash/splashcontroller.cpp" line="106"/>
+        <source>theDesk had a problem and was restarted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../splash/splashcontroller.cpp" line="108"/>
+        <source>View Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SplashWindow</name>
+    <message>
+        <location filename="../splashwindow.ui" line="104"/>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../splashwindow.ui" line="118"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../splashwindow.ui" line="129"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
