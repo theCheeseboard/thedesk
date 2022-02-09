5cc8a5e8a2b64adc649ee24b70d84588dde2098c
Weblate <noreply@weblate.org>
2022-02-09 12:08:51 +0000

    Added translation using Weblate (Danish)

---
+++ plugins/AccessibilityPlugin/translations/da.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="da">
+<context>
+    <name>AccessibilitySettingsPane</name>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="35"/>
+        <location filename="../settings/accessibilitysettingspane.cpp" line="66"/>
+        <source>Accessibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="84"/>
+        <source>Sticky Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="104"/>
+        <source>TYPING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="114"/>
+        <source>Allows you to press modifier keys sequentially</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="149"/>
+        <source>POINTING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="163"/>
+        <source>Mouse Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/accessibilitysettingspane.ui" line="173"/>
+        <source>Allows you to move the cursor using the number pad</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StickyKeysChunk</name>
+    <message>
+        <location filename="../chunks/stickykeyschunk.cpp" line="57"/>
+        <source>Disable Sticky Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
