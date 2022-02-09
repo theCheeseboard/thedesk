c2976cbf7bb17e6aeb483e24e3a033bcbe345f4b
Weblate <noreply@weblate.org>
2022-02-09 12:08:55 +0000

    Added translation using Weblate (Welsh)

---
+++ plugins/InputPlugin/translations/cy.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="cy">
+<context>
+    <name>InputSettingsLeftPane</name>
+    <message>
+        <location filename="../settings/inputsettingsleftpane.ui" line="35"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/inputsettingsleftpane.ui" line="46"/>
+        <source>Mouse and Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/inputsettingsleftpane.ui" line="55"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputSettingsPane</name>
+    <message>
+        <location filename="../settings/inputsettingspane.cpp" line="55"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardDaemon</name>
+    <message>
+        <location filename="../daemons/keyboarddaemon.cpp" line="83"/>
+        <source>Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardLayoutChunk</name>
+    <message>
+        <location filename="../chunks/keyboardlayoutchunk.cpp" line="75"/>
+        <source>Select Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardSettings</name>
+    <message>
+        <location filename="../settings/keyboardsettings.ui" line="35"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.ui" line="106"/>
+        <source>LAYOUTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.ui" line="113"/>
+        <source>Prioritise the keyboard layouts you&apos;d like to use on your device. You can switch between keyboard layouts with SUPER+Space</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.ui" line="135"/>
+        <source>Add Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.cpp" line="77"/>
+        <source>For this layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.cpp" line="78"/>
+        <source>Move Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.cpp" line="81"/>
+        <source>Move Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/keyboardsettings.cpp" line="84"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MouseSettings</name>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="35"/>
+        <source>Mouse and Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="105"/>
+        <source>CLICKING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="117"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="130"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="151"/>
+        <source>Primary Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="158"/>
+        <source>Tap to Click</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="204"/>
+        <source>Natural Scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="216"/>
+        <source>SCROLLING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="226"/>
+        <source>Reverse the direction that you scroll in so that the content moves in the same direction as your fingers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="278"/>
+        <source>Settings Unavailable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/mousesettings.ui" line="288"/>
+        <source>Mouse settings can&apos;t be changed on this platform.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
