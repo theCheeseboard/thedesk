546c85bf99605cbc5615ab1ef172b0f80fa14ab2
Weblate <noreply@weblate.org>
2022-02-09 12:09:19 +0000

    Added translation using Weblate (German)

---
+++ desktop-portal/translations/de.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="de">
+<context>
+    <name>FileChooserInterface</name>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileDialog</name>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overwrite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create New File Here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose This Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name this file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>What do you want to name the file?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overwrite?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A file named %1 already exists. Do you want to overwrite it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overwriting the file will replace its contents.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
