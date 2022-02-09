86156d9908df177d7b8f6d46ebaddd9265c100d7
Weblate <noreply@weblate.org>
2022-02-09 12:09:29 +0000

    Added translation using Weblate (Swedish)

---
+++ plugins/ScreenshotPlugin/translations/sv.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="sv">
+<context>
+    <name>PenButton</name>
+    <message>
+        <location filename="../penbutton.cpp" line="54"/>
+        <source>Erase-o</source>
+        <comment>This and the next translation span two lines. Be spiffy with these!</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../penbutton.cpp" line="55"/>
+        <source>matic 2000</source>
+        <comment>This and the previous translation span two lines. Be spiffy with these!</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotWindow</name>
+    <message>
+        <location filename="../screenshotwindow.ui" line="55"/>
+        <source>SCREENSHOT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="130"/>
+        <source>Crop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="150"/>
+        <source>Redact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="205"/>
+        <source>Reset Markup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="216"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="227"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
