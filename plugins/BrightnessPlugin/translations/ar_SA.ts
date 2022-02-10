5f67648dc1bd74e78ce4dcb05ff69d39ca68ba72
Weblate <noreply@weblate.org>
2022-02-10 14:54:43 +0000

    Added translation using Weblate (Arabic (Saudi Arabia))

---
+++ plugins/BrightnessPlugin/translations/ar_SA.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="ar_SA">
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../plugin.cpp" line="68"/>
+        <location filename="../plugin.cpp" line="74"/>
+        <location filename="../plugin.cpp" line="88"/>
+        <location filename="../plugin.cpp" line="94"/>
+        <source>Brightness</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
