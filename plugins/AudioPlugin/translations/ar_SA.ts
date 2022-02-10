4bd36a3502581ee22df4d0202b9a19aeebf42ed9
Weblate <noreply@weblate.org>
2022-02-10 14:54:40 +0000

    Added translation using Weblate (Arabic (Saudi Arabia))

---
+++ plugins/AudioPlugin/translations/ar_SA.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="ar_SA">
+<context>
+    <name>AudioQuickWidget</name>
+    <message>
+        <location filename="../audioquickwidget.ui" line="59"/>
+        <source>VOLUME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../audioquickwidget.ui" line="90"/>
+        <source>APPLICATIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../audioquickwidget.ui" line="109"/>
+        <source>QUIET MODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EventHandler</name>
+    <message>
+        <location filename="../eventhandler.cpp" line="92"/>
+        <source>No Audio Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="133"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="134"/>
+        <source>Unmute Quiet Mode before changing the volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="150"/>
+        <source>Speakers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="154"/>
+        <source>Headphones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="158"/>
+        <source>Line Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../eventhandler.cpp" line="166"/>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MicChunk</name>
+    <message numerus="yes">
+        <location filename="../micchunk.cpp" line="66"/>
+        <source>%n applications</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>QuickWidgetSink</name>
+    <message>
+        <location filename="../quickwidgetsink.ui" line="68"/>
+        <source>Make Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quickwidgetsink.ui" line="76"/>
+        <source>Use for All Applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuickWidgetSinkInput</name>
+    <message>
+        <location filename="../quickwidgetsinkinput.cpp" line="63"/>
+        <source>Play on</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
