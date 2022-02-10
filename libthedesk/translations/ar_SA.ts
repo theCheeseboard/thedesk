e29df9981139a1143e69bfd2ee5198c0d129f0ab
owahai <meero65@gmail.com>
2022-02-10 14:55:02 +0000

    Translated using Weblate (Arabic (Saudi Arabia))
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: theDesk/theDesk Core
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-core/ar_SA/

--- libthedesk/translations/ar_SA.ts
+++ libthedesk/translations/ar_SA.ts
@@ -5,110 +5,110 @@
     <name>KeyboardLayoutSelector</name>
     <message>
         <source>Select a keyboard layout</source>
-        <translation type="unfinished"></translation>
+        <translation>حدد تخطيط لوحة المفاتيح</translation>
     </message>
 </context>
 <context>
     <name>LocaleSelector</name>
     <message>
         <source>Select a locale</source>
-        <translation type="unfinished"></translation>
+        <translation>حدد لغة</translation>
     </message>
 </context>
 <context>
     <name>OnboardingFinal</name>
     <message>
         <source>That&apos;s everything!</source>
-        <translation type="unfinished"></translation>
+        <translation>هذا كل شئ!</translation>
     </message>
     <message>
         <source>We hope you enjoy using theDesk!</source>
-        <translation type="unfinished"></translation>
+        <translation>نأمل أن تستمتع باستخدام theDesk!</translation>
     </message>
     <message>
         <source>Get Started</source>
-        <translation type="unfinished"></translation>
+        <translation>ابدء</translation>
     </message>
     <message>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>تم</translation>
     </message>
 </context>
 <context>
     <name>OnboardingVideo</name>
     <message>
         <source>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</translation>
     </message>
 </context>
 <context>
     <name>OnboardingWelcome</name>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>الغة</translation>
     </message>
     <message>
         <source>Emergency</source>
-        <translation type="unfinished"></translation>
+        <translation>طوارئ</translation>
     </message>
     <message>
         <source>Let&apos;s go!</source>
-        <translation type="unfinished"></translation>
+        <translation>هيا نذهب!</translation>
     </message>
     <message>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>مرحبا</translation>
     </message>
     <message>
         <source>Welcome to %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>مرحباً بك في %1!</translation>
     </message>
     <message>
         <source>You&apos;ve made it! We&apos;ll quickly go through some important setup like setting up accounts and connecting to the Internet, and then you&apos;ll be up and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>لقد وصلت! سننتقل سريعًا إلى بعض الإعدادات المهمة مثل إعداد الحسابات والاتصال بالإنترنت ، وبعد ذلك ستكون جاهزاين للعمل.</translation>
     </message>
     <message>
         <source>Just a bit of important setup to do before you get started. This won&apos;t take long!</source>
-        <translation type="unfinished"></translation>
+        <translation>مجرد القليل من الإعداد المهم الذي يجب القيام به قبل العمل. هذا لن يأخذ وقت طويل!</translation>
     </message>
     <message>
         <source>OTHER OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>خيارات الأخرى</translation>
     </message>
 </context>
 <context>
     <name>QuietModeManagerTd</name>
     <message>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>الصوت</translation>
     </message>
     <message>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>المهمه فقط</translation>
     </message>
     <message>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>لا توجد اي إشعارات</translation>
     </message>
     <message>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>كتم الصوت</translation>
     </message>
     <message>
         <source>Allows all sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>يسمح بكل الأصوات</translation>
     </message>
     <message>
         <source>Hides non-critical notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>يخفي الإنظارات غير المهمه</translation>
     </message>
     <message>
         <source>Hides all notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>يخفي كل الإنظارات</translation>
     </message>
     <message>
         <source>Mutes all sound altogether</source>
-        <translation type="unfinished"></translation>
+        <translation>يكتم كل الأصوات تمامًا</translation>
     </message>
 </context>
 </TS>
