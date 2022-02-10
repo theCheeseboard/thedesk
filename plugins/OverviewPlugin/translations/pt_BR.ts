343330c5afe3253bdef59d18323c1b58b5a55052
Weblate <noreply@weblate.org>
2022-02-10 00:57:25 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ plugins/OverviewPlugin/translations/pt_BR.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="pt_BR">
+<context>
+    <name>OverviewPane</name>
+    <message>
+        <location filename="../OverviewPane/overviewpane.ui" line="102"/>
+        <source>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OverviewPane/overviewpane.cpp" line="108"/>
+        <source>Hi %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OverviewPane/overviewpane.cpp" line="110"/>
+        <source>Good morning, %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OverviewPane/overviewpane.cpp" line="112"/>
+        <source>Good afternoon, %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OverviewPane/overviewpane.cpp" line="114"/>
+        <source>Good evening, %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../OverviewPane/overviewpane.cpp" line="208"/>
+        <source>Daylight Savings Time is ending on %2. The clock will automatically shift backwards by %n hour(s).</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../OverviewPane/overviewpane.cpp" line="210"/>
+        <source>Daylight Savings Time is starting on %2. The clock will automatically shift forwards by %n hour(s).</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../OverviewPane/overviewpane.cpp" line="342"/>
+        <source>Overview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
