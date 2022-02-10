7ea7bd33db6802d06c400a58da4637668560630c
Weblate <noreply@weblate.org>
2022-02-10 14:54:37 +0000

    Added translation using Weblate (Arabic (Saudi Arabia))

---
+++ plugins/DisplayPlugin/translations/ar_SA.ts
@@ -0,0 +1,179 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="ar_SA">
+<context>
+    <name>ArrangeWidget</name>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="164"/>
+        <source>DISPLAY PROPERTIES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="100"/>
+        <source>Refresh Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="90"/>
+        <source>Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="141"/>
+        <source>Primary Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="110"/>
+        <source>Orientation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="118"/>
+        <source>Landscape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="123"/>
+        <source>Portrait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="128"/>
+        <source>Upside Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="133"/>
+        <source>Portrait (Upside Down)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="177"/>
+        <source>Enable Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.ui" line="194"/>
+        <source>Apply Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../arrange/arrangewidget.cpp" line="116"/>
+        <source>%1 hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplaySettings</name>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="35"/>
+        <location filename="../settings/displaysettings.cpp" line="102"/>
+        <source>Displays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="84"/>
+        <source>SCREENS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="91"/>
+        <source>Change the resolution of your display and arrange your displays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="101"/>
+        <source>Arrange and Resize Displays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="146"/>
+        <source>SCALING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="153"/>
+        <source>If items on the screen are too small, you can scale them up to make them easier to see.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="163"/>
+        <source>100% (96 DPI)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="170"/>
+        <source>125% (120 DPI)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="177"/>
+        <source>150% (144 DPI)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="184"/>
+        <source>200% (192 DPI)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="216"/>
+        <source>Schedule Redshift</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="229"/>
+        <source>REDSHIFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="236"/>
+        <source>Redshift blocks blue light so that you can sleep better at night</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="252"/>
+        <source>Intensity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="302"/>
+        <source>Follow Sunlight Cycle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="330"/>
+        <source>Start at</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="337"/>
+        <source>End at</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/displaysettings.ui" line="356"/>
+        <source>Your physical location will be sent to MET Norway to determine sunrise and sunset times.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RedshiftDaemon</name>
+    <message>
+        <location filename="../redshift/redshiftdaemon.cpp" line="86"/>
+        <source>Redshift Active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../redshift/redshiftdaemon.cpp" line="89"/>
+        <source>Disable Redshift until tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../redshift/redshiftdaemon.cpp" line="92"/>
+        <source>Display Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
