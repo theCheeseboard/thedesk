5f10b9380be1495bf5f39ac3682ff31ce45924c7
Weblate <noreply@weblate.org>
2022-02-10 00:57:22 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ plugins/TimeDatePlugin/translations/pt_BR.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="pt_BR">
+<context>
+    <name>DateTimePane</name>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="35"/>
+        <location filename="../settings/datetimepane.cpp" line="72"/>
+        <source>Date and Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="83"/>
+        <source>ACTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="107"/>
+        <source>Change Date and Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="114"/>
+        <source>Change Timezone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="142"/>
+        <source>Synchronise clock with the Internet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/datetimepane.ui" line="161"/>
+        <source>TIME SYNCHRONISATION</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OnboardingTimeZone</name>
+    <message>
+        <location filename="../onboarding/onboardingtimezone.ui" line="35"/>
+        <location filename="../onboarding/onboardingtimezone.cpp" line="53"/>
+        <source>Time Zone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingtimezone.ui" line="42"/>
+        <source>Whereabouts are you located? We&apos;ll use this information to display the current time.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingtimezone.ui" line="71"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingtimezone.ui" line="125"/>
+        <source>Use the Internet to synchronise the system clock (recommended)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingtimezone.ui" line="177"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SetTimeDatePopover</name>
+    <message>
+        <location filename="../popovers/settimedatepopover.ui" line="57"/>
+        <source>Set Date and Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/settimedatepopover.ui" line="64"/>
+        <source>What&apos;s the current date and time?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/settimedatepopover.ui" line="77"/>
+        <source>Apply Date and Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SetTimezonePopover</name>
+    <message>
+        <location filename="../popovers/settimezonepopover.ui" line="57"/>
+        <source>Set Timezone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/settimezonepopover.ui" line="64"/>
+        <source>Where are you now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../popovers/settimezonepopover.ui" line="90"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
