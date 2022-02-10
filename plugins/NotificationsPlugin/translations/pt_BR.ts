2c7b48d61e577123a18543383c205c08ec5146e8
Weblate <noreply@weblate.org>
2022-02-10 00:57:26 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ plugins/NotificationsPlugin/translations/pt_BR.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="pt_BR">
+<context>
+    <name>MprisChunk</name>
+    <message>
+        <location filename="../chunks/mprischunk.cpp" line="53"/>
+        <source>Media Players</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationAppGroup</name>
+    <message>
+        <location filename="../statuscenter/notificationappgroup.cpp" line="47"/>
+        <source>Uncategorised</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationsStatusCenterPane</name>
+    <message>
+        <location filename="../statuscenter/notificationsstatuscenterpane.ui" line="35"/>
+        <location filename="../statuscenter/notificationsstatuscenterpane.cpp" line="119"/>
+        <source>Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../statuscenter/notificationsstatuscenterpane.ui" line="51"/>
+        <source>QUIET MODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../statuscenter/notificationsstatuscenterpane.ui" line="113"/>
+        <source>Is it Friday yet?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../statuscenter/notificationsstatuscenterpane.ui" line="136"/>
+        <source>You&apos;ve caught up with your notifications!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
