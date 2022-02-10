a30938d553f1148e642916b9577fc2b0d7145e06
Weblate <noreply@weblate.org>
2022-02-10 10:37:38 +0000

    Added translation using Weblate (Indonesian)

---
+++ libthedesk/translations/id.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id" sourcelanguage="en_US">
+<context>
+    <name>KeyboardLayoutSelector</name>
+    <message>
+        <source>Select a keyboard layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocaleSelector</name>
+    <message>
+        <source>Select a locale</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OnboardingFinal</name>
+    <message>
+        <source>That&apos;s everything!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>We hope you enjoy using theDesk!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Get Started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OnboardingVideo</name>
+    <message>
+        <source>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OnboardingWelcome</name>
+    <message>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Emergency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Let&apos;s go!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome to %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You&apos;ve made it! We&apos;ll quickly go through some important setup like setting up accounts and connecting to the Internet, and then you&apos;ll be up and running.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Just a bit of important setup to do before you get started. This won&apos;t take long!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OTHER OPTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuietModeManagerTd</name>
+    <message>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Critical Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows all sounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hides non-critical notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hides all notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mutes all sound altogether</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
