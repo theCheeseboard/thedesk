d252c3d8f6eceffb837f2867e0e565dfb8eed7ab
Weblate <noreply@weblate.org>
2022-02-10 00:57:24 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ plugins/BluetoothPlugin/translations/pt_BR.ts
@@ -0,0 +1,354 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="pt_BR">
+<context>
+    <name>BluetoothChunk</name>
+    <message numerus="yes">
+        <location filename="../chunk/bluetoothchunk.cpp" line="60"/>
+        <source>%n devices connected</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothSettingsPane</name>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="35"/>
+        <location filename="../settings/bluetoothsettingspane.cpp" line="76"/>
+        <location filename="../settings/bluetoothsettingspane.cpp" line="181"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="106"/>
+        <source>ACTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="113"/>
+        <source>Pair New Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="139"/>
+        <source>Visible to other devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="166"/>
+        <source>DISCOVERABILITY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="213"/>
+        <source>DEVICES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="272"/>
+        <source>Bluetooth is unavailable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="295"/>
+        <source>Install a Bluetooth card to use Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="340"/>
+        <source>Bluetooth is disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="363"/>
+        <source>Switch Bluetooth on to connect to Bluetooth devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.ui" line="373"/>
+        <source>Enable Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/bluetoothsettingspane.cpp" line="119"/>
+        <source>To pair with this device, look for %1 on the other device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BtAgent</name>
+    <message>
+        <location filename="../btagent.cpp" line="89"/>
+        <location filename="../btagent.cpp" line="119"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="90"/>
+        <location filename="../btagent.cpp" line="120"/>
+        <source>Pair with %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="91"/>
+        <source>In order to complete pairing, confirm that the following pairing code is shown on %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="92"/>
+        <source>If it is, you can accept the pairing request.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="93"/>
+        <location filename="../btagent.cpp" line="122"/>
+        <source>Decline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="94"/>
+        <source>Accept and Pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="121"/>
+        <source>Accept the pairing request from %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btagent.cpp" line="123"/>
+        <source>Pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BtObex</name>
+    <message>
+        <location filename="../btobex.cpp" line="71"/>
+        <location filename="../btobex.cpp" line="111"/>
+        <location filename="../btobex.cpp" line="118"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="72"/>
+        <source>Incoming file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="73"/>
+        <source>%1 is sending you the file %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="74"/>
+        <source>Decline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="75"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="112"/>
+        <source>File received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="113"/>
+        <source>The file %1 has been received and can be found in your Downloads folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="119"/>
+        <source>File transfer failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../btobex.cpp" line="120"/>
+        <source>The file %1 was unable to be received.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Common</name>
+    <message>
+        <location filename="../common.cpp" line="29"/>
+        <source>Phone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="31"/>
+        <source>Modem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="33"/>
+        <source>Computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="35"/>
+        <source>Networking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="37"/>
+        <source>Headset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="39"/>
+        <source>Headphones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="41"/>
+        <source>Audio/Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="43"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="45"/>
+        <source>Mouse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="47"/>
+        <source>Gamepad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="49"/>
+        <source>Drawing Tablet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="51"/>
+        <source>Peripheral</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="53"/>
+        <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="55"/>
+        <source>Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="57"/>
+        <source>Imaging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="59"/>
+        <source>Wearable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="61"/>
+        <source>Toy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="63"/>
+        <source>Health</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../common.cpp" line="65"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DevicePopover</name>
+    <message>
+        <location filename="../settings/devicepopover.ui" line="124"/>
+        <source>ACTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/devicepopover.ui" line="133"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/devicepopover.ui" line="140"/>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/devicepopover.ui" line="147"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/devicepopover.cpp" line="72"/>
+        <source>Failed to connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/devicepopover.cpp" line="75"/>
+        <source>Connecting to %1 failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PairPopover</name>
+    <message>
+        <location filename="../settings/pairpopover.ui" line="57"/>
+        <source>Pair Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.ui" line="64"/>
+        <source>To get started, ensure the device you want to pair is visible. This may be achieved by opening the Bluetooth settings or pressing a button on the other device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.ui" line="383"/>
+        <source>Confirm Pairing Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="92"/>
+        <source>Then press ENTER or RETURN.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="130"/>
+        <source>Failed to pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="133"/>
+        <source>Pairing with %1 failed because the pairing code was incorrect.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="136"/>
+        <source>Pairing with %1 failed because the device rejected the pairing request.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="139"/>
+        <source>Pairing with %1 failed because the pairing code was not confirmed in time.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/pairpopover.cpp" line="142"/>
+        <source>Pairing with %1 failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
