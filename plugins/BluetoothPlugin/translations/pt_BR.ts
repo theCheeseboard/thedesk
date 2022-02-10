5a9ed5ddecaf00a6026d51a13908c4cfe285cd8e
Vrabbers <nininininimocp@gmail.com>
2022-02-10 01:24:56 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: theDesk/theDesk Bluetooth
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-bluetooth/pt_BR/

--- plugins/BluetoothPlugin/translations/pt_BR.ts
+++ plugins/BluetoothPlugin/translations/pt_BR.ts
@@ -6,9 +6,9 @@
     <message numerus="yes">
         <location filename="../chunk/bluetoothchunk.cpp" line="60"/>
         <source>%n devices connected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n dispositivo conectado</numerusform>
+            <numerusform>%n dispositivos conectados</numerusform>
         </translation>
     </message>
 </context>
@@ -19,62 +19,62 @@
         <location filename="../settings/bluetoothsettingspane.cpp" line="76"/>
         <location filename="../settings/bluetoothsettingspane.cpp" line="181"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="106"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>AÇÕES</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="113"/>
         <source>Pair New Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Emparelhar um novo dispositivo</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="139"/>
         <source>Visible to other devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Visível a outros dispositivos</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="166"/>
         <source>DISCOVERABILITY</source>
-        <translation type="unfinished"></translation>
+        <translation>VISIBILIDADE</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="213"/>
         <source>DEVICES</source>
-        <translation type="unfinished"></translation>
+        <translation>DISPOSITIVOS</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="272"/>
         <source>Bluetooth is unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth está indisponível</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="295"/>
         <source>Install a Bluetooth card to use Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Instale um adaptador Bluetooth para usar o Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="340"/>
         <source>Bluetooth is disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth está desabilitado</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="363"/>
         <source>Switch Bluetooth on to connect to Bluetooth devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Ligue o Bluetooth para conectar dispositivos Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.ui" line="373"/>
         <source>Enable Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Ativar o Bluetooth</translation>
     </message>
     <message>
         <location filename="../settings/bluetoothsettingspane.cpp" line="119"/>
         <source>To pair with this device, look for %1 on the other device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Para emparelhar com este dispositivo, procure por %1 no outro dispositivo.</translation>
     </message>
 </context>
 <context>
@@ -83,44 +83,44 @@
         <location filename="../btagent.cpp" line="89"/>
         <location filename="../btagent.cpp" line="119"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="90"/>
         <location filename="../btagent.cpp" line="120"/>
         <source>Pair with %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Emparelhar com %1?</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="91"/>
         <source>In order to complete pairing, confirm that the following pairing code is shown on %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Para completar o emparelhamento, confirme que o seguinte código está sendo exibido em %1.</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="92"/>
         <source>If it is, you can accept the pairing request.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se for, você pode aceitar o pedido de emparelhamento.</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="93"/>
         <location filename="../btagent.cpp" line="122"/>
         <source>Decline</source>
-        <translation type="unfinished"></translation>
+        <translation>Recusar</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="94"/>
         <source>Accept and Pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Aceitar e emparelhar</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="121"/>
         <source>Accept the pairing request from %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Aceitar o pedido de emparelhamento de %1?</translation>
     </message>
     <message>
         <location filename="../btagent.cpp" line="123"/>
         <source>Pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Emparelhar</translation>
     </message>
 </context>
 <context>
@@ -130,47 +130,47 @@
         <location filename="../btobex.cpp" line="111"/>
         <location filename="../btobex.cpp" line="118"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="72"/>
         <source>Incoming file</source>
-        <translation type="unfinished"></translation>
+        <translation>Arquivo a ser recebido</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="73"/>
         <source>%1 is sending you the file %2.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 está mandando o arquivo %2.</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="74"/>
         <source>Decline</source>
-        <translation type="unfinished"></translation>
+        <translation>Recusar</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="75"/>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>Aceitar</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="112"/>
         <source>File received</source>
-        <translation type="unfinished"></translation>
+        <translation>Arquivo recebido</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="113"/>
         <source>The file %1 has been received and can be found in your Downloads folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>O arquivo %1 foi recebido e pode ser encontrado na pasta Downloads.</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="119"/>
         <source>File transfer failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha na transferência</translation>
     </message>
     <message>
         <location filename="../btobex.cpp" line="120"/>
         <source>The file %1 was unable to be received.</source>
-        <translation type="unfinished"></translation>
+        <translation>Não foi possível receber o arquivo %1.</translation>
     </message>
 </context>
 <context>
@@ -178,97 +178,97 @@
     <message>
         <location filename="../common.cpp" line="29"/>
         <source>Phone</source>
-        <translation type="unfinished"></translation>
+        <translation>Telefone</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="31"/>
         <source>Modem</source>
-        <translation type="unfinished"></translation>
+        <translation>Modem</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="33"/>
         <source>Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Computador</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="35"/>
         <source>Networking</source>
-        <translation type="unfinished"></translation>
+        <translation>Rede</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="37"/>
         <source>Headset</source>
-        <translation type="unfinished"></translation>
+        <translation>Headset</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="39"/>
         <source>Headphones</source>
-        <translation type="unfinished"></translation>
+        <translation>Fones de ouvido</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="41"/>
         <source>Audio/Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Áudio/Vídeo</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="43"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Teclado</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="45"/>
         <source>Mouse</source>
-        <translation type="unfinished"></translation>
+        <translation>Mouse</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="47"/>
         <source>Gamepad</source>
-        <translation type="unfinished"></translation>
+        <translation>Gamepad</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="49"/>
         <source>Drawing Tablet</source>
-        <translation type="unfinished"></translation>
+        <translation>Mesa digitalizadora</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="51"/>
         <source>Peripheral</source>
-        <translation type="unfinished"></translation>
+        <translation>Periférico</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="53"/>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Câmera</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="55"/>
         <source>Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Impressora</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="57"/>
         <source>Imaging</source>
-        <translation type="unfinished"></translation>
+        <translation>Dispositivo de imagem</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="59"/>
         <source>Wearable</source>
-        <translation type="unfinished"></translation>
+        <translation>Acessório wearable</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="61"/>
         <source>Toy</source>
-        <translation type="unfinished"></translation>
+        <translation>Brinquedo</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="63"/>
         <source>Health</source>
-        <translation type="unfinished"></translation>
+        <translation>Saúde</translation>
     </message>
     <message>
         <location filename="../common.cpp" line="65"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconhecido</translation>
     </message>
 </context>
 <context>
@@ -276,32 +276,32 @@
     <message>
         <location filename="../settings/devicepopover.ui" line="124"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>AÇÕES</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="133"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Conectar</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="140"/>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconectar</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.ui" line="147"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Remover</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.cpp" line="72"/>
         <source>Failed to connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao conectar</translation>
     </message>
     <message>
         <location filename="../settings/devicepopover.cpp" line="75"/>
         <source>Connecting to %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao conectar-se com %1.</translation>
     </message>
 </context>
 <context>
@@ -309,47 +309,47 @@
     <message>
         <location filename="../settings/pairpopover.ui" line="57"/>
         <source>Pair Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Emparelhar dispositivo</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.ui" line="64"/>
         <source>To get started, ensure the device you want to pair is visible. This may be achieved by opening the Bluetooth settings or pressing a button on the other device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Certifique-se que o dispositivo que você quer emparelhar está visível. Pode ser necessário abrir as configurações Bluetooth ou apertar um botão no outro dispositivo.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.ui" line="383"/>
         <source>Confirm Pairing Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirme o código de emparelhamento</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="92"/>
         <source>Then press ENTER or RETURN.</source>
-        <translation type="unfinished"></translation>
+        <translation>Então aperte ENTER.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="130"/>
         <source>Failed to pair</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao emparelhar</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="133"/>
         <source>Pairing with %1 failed because the pairing code was incorrect.</source>
-        <translation type="unfinished"></translation>
+        <translation>O emparelhamento com %1 falhou porque o código de emparelhamento estava incorreto.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="136"/>
         <source>Pairing with %1 failed because the device rejected the pairing request.</source>
-        <translation type="unfinished"></translation>
+        <translation>O emparelhamento com %1 falhou porque o dispositivo rejeitou a solicitação de emparelhamento.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="139"/>
         <source>Pairing with %1 failed because the pairing code was not confirmed in time.</source>
-        <translation type="unfinished"></translation>
+        <translation>O emparelhamento com %1 falhou porque o código de emparelhamento não foi confirmado a tempo.</translation>
     </message>
     <message>
         <location filename="../settings/pairpopover.cpp" line="142"/>
         <source>Pairing with %1 failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>O emparelhamento com %1 falhou.</translation>
     </message>
 </context>
 </TS>
