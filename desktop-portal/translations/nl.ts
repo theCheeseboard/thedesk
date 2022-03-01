0af1c44756d189fdad8570c8e2e4b5421e658bd8
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:12:07 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: theDesk/theDesk Desktop Portal
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-desktop-portal/nl/

--- desktop-portal/translations/nl.ts
+++ desktop-portal/translations/nl.ts
@@ -5,58 +5,58 @@
     <name>FileChooserInterface</name>
     <message>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Openen</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Opslaan</translation>
     </message>
 </context>
 <context>
     <name>FileDialog</name>
     <message>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Openen</translation>
     </message>
     <message>
         <source>Overwrite</source>
-        <translation type="unfinished"></translation>
+        <translation>Overschrijven</translation>
     </message>
     <message>
         <source>Save Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier Opslaan</translation>
     </message>
     <message>
         <source>Create New File Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier een Nieuw Bestand Maken</translation>
     </message>
     <message>
         <source>Open Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Map Openen</translation>
     </message>
     <message>
         <source>Choose This Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze Map Kiezen</translation>
     </message>
     <message>
         <source>Name this file</source>
-        <translation type="unfinished"></translation>
+        <translation>Geef dit bestand een naam</translation>
     </message>
     <message>
         <source>What do you want to name the file?</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoe wil je dit bestand noemen?</translation>
     </message>
     <message>
         <source>Overwrite?</source>
-        <translation type="unfinished"></translation>
+        <translation>Overschrijven?</translation>
     </message>
     <message>
         <source>A file named %1 already exists. Do you want to overwrite it?</source>
-        <translation type="unfinished"></translation>
+        <translation>Er bestaat al een bestand met de naam %1. Wil je deze overschrijven?</translation>
     </message>
     <message>
         <source>Overwriting the file will replace its contents.</source>
-        <translation type="unfinished"></translation>
+        <translation>Het bestand overschrijven zal de inhoud ervan vervangen.</translation>
     </message>
 </context>
 </TS>
