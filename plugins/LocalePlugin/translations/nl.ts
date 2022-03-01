fdb5a8e943ff7965f8a53897e9b1cf918a0c0e41
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:26:24 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: theDesk/theDesk Locale
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-locale/nl/

--- plugins/LocalePlugin/translations/nl.ts
+++ plugins/LocalePlugin/translations/nl.ts
@@ -7,107 +7,107 @@
         <location filename="../settings/localesettingspane.ui" line="35"/>
         <location filename="../settings/localesettingspane.cpp" line="119"/>
         <source>Locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Taal</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="107"/>
         <source>LANGUAGE</source>
-        <translation type="unfinished"></translation>
+        <translation>TAAL</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="114"/>
         <source>Prioritise the languages you&apos;d like to use on your device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Geef de talen die je op te apparaat wil gebruiken prioriteit.</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="133"/>
         <source>Add Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Taal Toevoegen</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="277"/>
         <source>Currency</source>
-        <translation type="unfinished"></translation>
+        <translation>Munteenheid</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="221"/>
         <source>Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Tijd</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="242"/>
         <source>Measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Lengtes</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="263"/>
         <source>Country</source>
-        <translation type="unfinished"></translation>
+        <translation>Land</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="184"/>
         <source>Select your preferred regional formats</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecteer de regionale formaten van je voorkeur</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="177"/>
         <source>Numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Getallen</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="228"/>
         <source>First day of week</source>
-        <translation type="unfinished"></translation>
+        <translation>Eerste dag van de week</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="207"/>
         <source>REGION</source>
-        <translation type="unfinished"></translation>
+        <translation>REGIO</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.ui" line="256"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="78"/>
         <source>Primary</source>
-        <translation type="unfinished"></translation>
+        <translation>Primair</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="95"/>
         <source>Metric</source>
-        <translation type="unfinished"></translation>
+        <translation>Metriek</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="98"/>
         <source>Imperial</source>
-        <translation type="unfinished"></translation>
+        <translation>Amerikaans</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="101"/>
         <source>Imperial (United Kingdom)</source>
-        <translation type="unfinished"></translation>
+        <translation>Brits</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="152"/>
         <source>For this language</source>
-        <translation type="unfinished"></translation>
+        <translation>Voor deze taal</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="153"/>
         <source>Move Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Omhoog</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="156"/>
         <source>Move Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Omlaag</translation>
     </message>
     <message>
         <location filename="../settings/localesettingspane.cpp" line="159"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Weghalen</translation>
     </message>
 </context>
 <context>
@@ -116,22 +116,22 @@
         <location filename="../onboarding/onboardingregion.ui" line="35"/>
         <location filename="../onboarding/onboardingregion.cpp" line="52"/>
         <source>Region</source>
-        <translation type="unfinished"></translation>
+        <translation>Regio</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingregion.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to format your dates, numbers and other items correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wat is je verblijfplaats? Deze informatie gebruiken we om je data, getallen en andere dingen juist weer te geven.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingregion.ui" line="71"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoeken...</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingregion.ui" line="138"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Volgende</translation>
     </message>
 </context>
 </TS>
