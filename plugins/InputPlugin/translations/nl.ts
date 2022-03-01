bac7b50de0e6aa1d62d7f76d841fe99b7abbbc0a
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:33:03 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: theDesk/theDesk Input Devices
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-input-devices/nl/

--- plugins/InputPlugin/translations/nl.ts
+++ plugins/InputPlugin/translations/nl.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../settings/inputsettingsleftpane.ui" line="35"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Input</translation>
     </message>
     <message>
         <location filename="../settings/inputsettingsleftpane.ui" line="46"/>
         <source>Mouse and Touchpad</source>
-        <translation type="unfinished"></translation>
+        <translation>Muis en Touchpad</translation>
     </message>
     <message>
         <location filename="../settings/inputsettingsleftpane.ui" line="55"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Toetsenbord</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../settings/inputsettingspane.cpp" line="55"/>
         <source>Input</source>
-        <translation type="unfinished"></translation>
+        <translation>Input</translation>
     </message>
 </context>
 <context>
@@ -32,7 +32,7 @@
     <message>
         <location filename="../daemons/keyboarddaemon.cpp" line="83"/>
         <source>Keyboard Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Toetsenbordindeling</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../chunks/keyboardlayoutchunk.cpp" line="75"/>
         <source>Select Keyboard Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Toetsenbordindeling Selecteren</translation>
     </message>
 </context>
 <context>
@@ -48,42 +48,42 @@
     <message>
         <location filename="../settings/keyboardsettings.ui" line="35"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Toetsenbord</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.ui" line="106"/>
         <source>LAYOUTS</source>
-        <translation type="unfinished"></translation>
+        <translation>INDELINGEN</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.ui" line="113"/>
         <source>Prioritise the keyboard layouts you&apos;d like to use on your device. You can switch between keyboard layouts with SUPER+Space</source>
-        <translation type="unfinished"></translation>
+        <translation>Geef de toetsenbordindelingen die je op je apparaat wil gebruiken prioriteit. Je kunt tussen indelingen wisselen met SUPER+Spatie</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.ui" line="135"/>
         <source>Add Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Indeling Toevoegen</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.cpp" line="77"/>
         <source>For this layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Voor deze indeling</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.cpp" line="78"/>
         <source>Move Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Omhoog</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.cpp" line="81"/>
         <source>Move Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Omlaag</translation>
     </message>
     <message>
         <location filename="../settings/keyboardsettings.cpp" line="84"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Weghalen</translation>
     </message>
 </context>
 <context>
@@ -91,57 +91,57 @@
     <message>
         <location filename="../settings/mousesettings.ui" line="35"/>
         <source>Mouse and Touchpad</source>
-        <translation type="unfinished"></translation>
+        <translation>Muis en Touchpad</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="105"/>
         <source>CLICKING</source>
-        <translation type="unfinished"></translation>
+        <translation>KLIKKEN</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="117"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Links</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="130"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Rechts</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="151"/>
         <source>Primary Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Primaire Knop</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="158"/>
         <source>Tap to Click</source>
-        <translation type="unfinished"></translation>
+        <translation>Aanraken om te Klikken</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="204"/>
         <source>Natural Scrolling</source>
-        <translation type="unfinished"></translation>
+        <translation>Natuurlijk Scrollen</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="216"/>
         <source>SCROLLING</source>
-        <translation type="unfinished"></translation>
+        <translation>SCROLLEN</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="226"/>
         <source>Reverse the direction that you scroll in so that the content moves in the same direction as your fingers.</source>
-        <translation type="unfinished"></translation>
+        <translation>Draai de richting waarin je scrolt om, zodat de inhoud dezelfde kant op beweegt als je vingers.</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="278"/>
         <source>Settings Unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen Niet Beschikbaar</translation>
     </message>
     <message>
         <location filename="../settings/mousesettings.ui" line="288"/>
         <source>Mouse settings can&apos;t be changed on this platform.</source>
-        <translation type="unfinished"></translation>
+        <translation>Muisinstellingen kunnen niet worden aangepast om dit platform.</translation>
     </message>
 </context>
 </TS>
