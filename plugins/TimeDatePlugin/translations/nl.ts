f0f9fbf1d3e63dd36e43888f35b93f47fb2ed7ac
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:23:42 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: theDesk/theDesk Time and Date
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-time-and-date/nl/

--- plugins/TimeDatePlugin/translations/nl.ts
+++ plugins/TimeDatePlugin/translations/nl.ts
@@ -7,32 +7,32 @@
         <location filename="../settings/datetimepane.ui" line="35"/>
         <location filename="../settings/datetimepane.cpp" line="72"/>
         <source>Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum en Tijd</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="83"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>ACTIES</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="107"/>
         <source>Change Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum en Tijd Wijzigen</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="114"/>
         <source>Change Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Tijdzone Wijzigen</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="142"/>
         <source>Synchronise clock with the Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Klok met het Internet synchroniseren</translation>
     </message>
     <message>
         <location filename="../settings/datetimepane.ui" line="161"/>
         <source>TIME SYNCHRONISATION</source>
-        <translation type="unfinished"></translation>
+        <translation>TIJDSYNCHRONISATIE</translation>
     </message>
 </context>
 <context>
@@ -41,27 +41,27 @@
         <location filename="../onboarding/onboardingtimezone.ui" line="35"/>
         <location filename="../onboarding/onboardingtimezone.cpp" line="53"/>
         <source>Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Tijdzone</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="42"/>
         <source>Whereabouts are you located? We&apos;ll use this information to display the current time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wat is je verblijfplaats? Deze informatie gebruiken we om de huidige tijd weer te geven.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="71"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoeken</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="125"/>
         <source>Use the Internet to synchronise the system clock (recommended)</source>
-        <translation type="unfinished"></translation>
+        <translation>Het Internet gebruiken om de systeemklok te synchroniseren (aangeraden)</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtimezone.ui" line="177"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Volgende</translation>
     </message>
 </context>
 <context>
@@ -69,17 +69,17 @@
     <message>
         <location filename="../popovers/settimedatepopover.ui" line="57"/>
         <source>Set Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum en Tijd Instellen</translation>
     </message>
     <message>
         <location filename="../popovers/settimedatepopover.ui" line="64"/>
         <source>What&apos;s the current date and time?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wat is de huidige datum en tijd?</translation>
     </message>
     <message>
         <location filename="../popovers/settimedatepopover.ui" line="77"/>
         <source>Apply Date and Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum en Tijd Toepassen</translation>
     </message>
 </context>
 <context>
@@ -87,17 +87,17 @@
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="57"/>
         <source>Set Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Tijdzone Instellen</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="64"/>
         <source>Where are you now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Waar ben je nu?</translation>
     </message>
     <message>
         <location filename="../popovers/settimezonepopover.ui" line="90"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoeken</translation>
     </message>
 </context>
 </TS>
