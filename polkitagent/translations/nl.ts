03ef960eb38f6d0c15f3f5f1bec1c6358ffb78aa
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:02:47 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: theDesk/theDesk Polkit
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-polkit/nl/

--- polkitagent/translations/nl.ts
+++ polkitagent/translations/nl.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../authwindow.ui" line="35"/>
         <source>PolicyKit Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>PolicyKit-authenticatie</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="199"/>
         <source>Enter your password to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Voer je wachtwoord in om deze actie te bevestigen</translation>
     </message>
     <message>
         <location filename="../authwindow.ui" line="441"/>
         <source>Pass the authentication challenge to confirm this action</source>
-        <translation type="unfinished"></translation>
+        <translation>Haal de authenticatie-uitdaging om deze actie te bevestigen</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="159"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout</translation>
     </message>
     <message>
         <location filename="../authwindow.cpp" line="166"/>
         <source>Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Bericht</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../identitypopover.ui" line="35"/>
         <source>Identity</source>
-        <translation type="unfinished"></translation>
+        <translation>Identiteit</translation>
     </message>
     <message>
         <location filename="../identitypopover.ui" line="42"/>
         <source>Who do you want to authenticate as?</source>
-        <translation type="unfinished"></translation>
+        <translation>Als wie wil je je authentificeren?</translation>
     </message>
 </context>
 </TS>
