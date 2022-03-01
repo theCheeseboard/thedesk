fa810dddd67ad7ac8c24f26014739e79416cd9cf
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 12:54:51 +0000

    Translated using Weblate (Dutch)
    Currently translated at 88.2% (15 of 17 strings)
    
    Translation: theDesk/theDesk Session
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-session/nl/

--- startdesk/translations/nl.ts
+++ startdesk/translations/nl.ts
@@ -7,7 +7,7 @@
         <location filename="../crash/backtracedialog.ui" line="14"/>
         <location filename="../crash/backtracedialog.ui" line="35"/>
         <source>Error Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Foutdetails</translation>
     </message>
 </context>
 <context>
@@ -15,7 +15,7 @@
     <message>
         <location filename="../crash/backtracepopover.ui" line="35"/>
         <source>Error Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Foutdetails</translation>
     </message>
 </context>
 <context>
@@ -23,17 +23,17 @@
     <message>
         <location filename="../crash/backtracewidget.ui" line="50"/>
         <source>Review the information below and ensure it does not contain any personal data. Attach the information to a bug report and we&apos;ll get back to you!</source>
-        <translation type="unfinished"></translation>
+        <translation>Bekijk de gegevens hieronder en zorg ervoor dat er geen persoonlijke gegevens in staan. Voeg deze informatie toe aan een bugrapport en dan komen we bij je terug!</translation>
     </message>
     <message>
         <location filename="../crash/backtracewidget.ui" line="66"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>ACTIES</translation>
     </message>
     <message>
         <location filename="../crash/backtracewidget.ui" line="73"/>
         <source>Save to file</source>
-        <translation type="unfinished"></translation>
+        <translation>Opslaan naar bestand</translation>
     </message>
 </context>
 <context>
@@ -41,33 +41,33 @@
     <message>
         <location filename="../crash/crashwidget.ui" line="38"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Oh nee!</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="61"/>
         <location filename="../crash/crashwidget.cpp" line="36"/>
         <source>theDesk had a problem and has stopped working.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk had een probleem en werkt niet meer.</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="86"/>
         <source>Relaunch theDesk</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk Herstarten</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="97"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Uitloggen</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.ui" line="151"/>
         <source>View Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details Weergeven</translation>
     </message>
     <message>
         <location filename="../crash/crashwidget.cpp" line="39"/>
         <source>theDesk couldn&apos;t start because of a problem.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk kon niet opstarten door een probleem.</translation>
     </message>
 </context>
 <context>
@@ -75,17 +75,17 @@
     <message>
         <location filename="../splash/splashcontroller.cpp" line="105"/>
         <source>Oh, Bonkers!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Oh nee!</translation>
     </message>
     <message>
         <location filename="../splash/splashcontroller.cpp" line="106"/>
         <source>theDesk had a problem and was restarted.</source>
-        <translation type="unfinished"></translation>
+        <translation>theDesk had een probleem en is herstart.</translation>
     </message>
     <message>
         <location filename="../splash/splashcontroller.cpp" line="108"/>
         <source>View Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details Weergeven</translation>
     </message>
 </context>
 <context>
@@ -93,17 +93,17 @@
     <message>
         <location filename="../splashwindow.ui" line="104"/>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Vraag</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="118"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja</translation>
     </message>
     <message>
         <location filename="../splashwindow.ui" line="129"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nee</translation>
     </message>
 </context>
 </TS>
