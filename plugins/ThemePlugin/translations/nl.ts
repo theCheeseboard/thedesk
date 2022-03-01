a3dd7cc942d3d71ba8fc2bd27703605af6a6a573
Mart Koster <the947thtokay@codingtime.nl>
2022-03-01 13:37:36 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: theDesk/theDesk Theme
    Translate-URL: https://translate.vicr123.com/projects/thedesk/thedesk-theme/nl/

--- plugins/ThemePlugin/translations/nl.ts
+++ plugins/ThemePlugin/translations/nl.ts
@@ -7,37 +7,37 @@
         <location filename="../onboarding/onboardingtheme.ui" line="35"/>
         <location filename="../onboarding/onboardingtheme.cpp" line="91"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thema</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="42"/>
         <source>Select a theme. You&apos;ll be able to change this at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecteer een thema. Je kunt deze altijd aanpassen.</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="92"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Donker</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="108"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Licht</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.ui" line="241"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Volgende</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="76"/>
         <source>Intellect has spoken; blinding shall be no more!</source>
-        <translation type="unfinished"></translation>
+        <translation>Kennis heeft gesproken; verblinding is voorbij!</translation>
     </message>
     <message>
         <location filename="../onboarding/onboardingtheme.cpp" line="81"/>
         <source>One whose wisdom has brought them the light of day!</source>
-        <translation type="unfinished"></translation>
+        <translation>Een waarvan de wijsheid het daglicht heeft gebracht!</translation>
     </message>
 </context>
 <context>
@@ -46,132 +46,132 @@
         <location filename="../settings/themesettingspane.ui" line="35"/>
         <location filename="../settings/themesettingspane.cpp" line="198"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thema</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="127"/>
         <source>Accent Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Accentkleur</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="141"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Donker</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="146"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Licht</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="154"/>
         <source>Change the colours of the interface</source>
-        <translation type="unfinished"></translation>
+        <translation>De kleuren van de interface veranderen</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="161"/>
         <source>Base Colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Basiskleur</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="174"/>
         <source>COLOURS</source>
-        <translation type="unfinished"></translation>
+        <translation>KLEUREN</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="224"/>
         <source>WINDOW BORDERS</source>
-        <translation type="unfinished"></translation>
+        <translation>VENSTERRANDEN</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="231"/>
         <source>If the window borders don&apos;t look correct, we can fix that up for you. This will also change your window borders in KDE, and any other desktop that uses KWin for the window manager.</source>
-        <translation type="unfinished"></translation>
+        <translation>Als de vensterranden er niet goed uitzien, kunnen we dat voor je oplossen. Dat verandert ook de vensterranden in KDE en alle andere desktops die KWin als window manager gebruiken.</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="243"/>
         <source>Set Window Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Vensterranden Instellen</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="274"/>
         <source>Fixed Width Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Lettertype met Gelijke Breedte</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="281"/>
         <source>Interface Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Lettertype voor Interface</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="304"/>
         <source>FONTS</source>
-        <translation type="unfinished"></translation>
+        <translation>LETTERTYPES</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="363"/>
         <source>Set the appearance of buttons, text boxes and other interface elements</source>
-        <translation type="unfinished"></translation>
+        <translation>Het uiterlijk van knoppen, tekstvakken en andere interface-elementen aanpassen</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="370"/>
         <source>Widget Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Widgetthema</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="383"/>
         <source>WIDGETS</source>
-        <translation type="unfinished"></translation>
+        <translation>WIDGETS</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="434"/>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Knop</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="447"/>
         <source>Radio Button 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Radio-knop 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="457"/>
         <source>Using the theme set above, interface elements will look like this:</source>
-        <translation type="unfinished"></translation>
+        <translation>Met het thema van hierboven zien interface-elementen er zo uit:</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="464"/>
         <source>Radio Button 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Radio-knop 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="471"/>
         <source>Check Box 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Checkbox 2</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="487"/>
         <source>DEMO</source>
-        <translation type="unfinished"></translation>
+        <translation>DEMO</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="494"/>
         <source>Check Box 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Checkbox 1</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="508"/>
         <source>Type here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Type hier!</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="543"/>
         <source>Transparency Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Transparantie-effecten</translation>
     </message>
     <message>
         <location filename="../settings/themesettingspane.ui" line="563"/>
         <source>EFFECTS</source>
-        <translation type="unfinished"></translation>
+        <translation>EFFECTEN</translation>
     </message>
 </context>
 </TS>
