6fe718c8b8772a49125db8ccf956f1dffec777ce
Weblate <noreply@weblate.org>
2022-06-30 05:23:11 +0000

    Added translation using Weblate (Spanish)

---
+++ plugins/ScreenshotPlugin/translations/es_VE.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="es_VE">
+<context>
+    <name>PenButton</name>
+    <message>
+        <location filename="../penbutton.cpp" line="54"/>
+        <source>Erase-o</source>
+        <comment>This and the next translation span two lines. Be spiffy with these!</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../penbutton.cpp" line="55"/>
+        <source>matic 2000</source>
+        <comment>This and the previous translation span two lines. Be spiffy with these!</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotWindow</name>
+    <message>
+        <location filename="../screenshotwindow.ui" line="55"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="192"/>
+        <source>SCREENSHOT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="130"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="193"/>
+        <source>Crop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="150"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="194"/>
+        <source>Redact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="205"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="195"/>
+        <source>Reset Markup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="216"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="196"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screenshotwindow.ui" line="227"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/ScreenshotPlugin/plugin-screenshot_autogen/include/ui_screenshotwindow.h" line="197"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
