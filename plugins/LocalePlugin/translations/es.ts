05b4f772ae73eb043e8118c1c861c40c24348d1f
Weblate <noreply@weblate.org>
2022-10-21 01:30:00 +0000

    Added translation using Weblate (Spanish)

---
+++ plugins/LocalePlugin/translations/es.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en_US">
+<context>
+    <name>LocaleSettingsPane</name>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="35"/>
+        <location filename="../settings/localesettingspane.cpp" line="118"/>
+        <source>Locale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="107"/>
+        <source>LANGUAGE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="114"/>
+        <source>Prioritise the languages you&apos;d like to use on your device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="133"/>
+        <source>Add Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="277"/>
+        <source>Currency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="221"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="242"/>
+        <source>Measurement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="263"/>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="184"/>
+        <source>Select your preferred regional formats</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="177"/>
+        <source>Numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="228"/>
+        <source>First day of week</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="207"/>
+        <source>REGION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.ui" line="256"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="78"/>
+        <source>Primary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="95"/>
+        <source>Metric</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="98"/>
+        <source>Imperial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="101"/>
+        <source>Imperial (United Kingdom)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="151"/>
+        <source>For this language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="152"/>
+        <source>Move Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="155"/>
+        <source>Move Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings/localesettingspane.cpp" line="158"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OnboardingRegion</name>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="35"/>
+        <location filename="../onboarding/onboardingregion.cpp" line="52"/>
+        <source>Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="42"/>
+        <source>Whereabouts are you located? We&apos;ll use this information to format your dates, numbers and other items correctly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="71"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../onboarding/onboardingregion.ui" line="138"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
