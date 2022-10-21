c3d63f717587d7e5e62e513ceaea239370c09049
Weblate <noreply@weblate.org>
2022-10-21 01:30:19 +0000

    Added translation using Weblate (Spanish)

---
+++ plugins/DefaultsPlugin/translations/es.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="es">
+<context>
+    <name>DefaultsPane</name>
+    <message>
+        <location filename="../defaultspane.ui" line="35"/>
+        <location filename="../defaultspane.cpp" line="81"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/DefaultsPlugin/plugin-defaults_autogen/include/ui_defaultspane.h" line="102"/>
+        <source>Default Applications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="54"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/DefaultsPlugin/plugin-defaults_autogen/include/ui_defaultspane.h" line="103"/>
+        <source>Web Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="61"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/DefaultsPlugin/plugin-defaults_autogen/include/ui_defaultspane.h" line="104"/>
+        <source>Set your default apps below</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../defaultspane.ui" line="68"/>
+        <location filename="../../../../build-thedesk-Chroot_6-Debug/plugins/DefaultsPlugin/plugin-defaults_autogen/include/ui_defaultspane.h" line="105"/>
+        <source>Mail Client</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
