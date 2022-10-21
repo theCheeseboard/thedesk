c86684183fac6f25f60a61fde55ffbc91eac0bd3
Weblate <noreply@weblate.org>
2022-10-21 01:30:01 +0000

    Added translation using Weblate (Spanish)

---
+++ platform/translations/es.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="es">
+<context>
+    <name>MessageDialog</name>
+    <message>
+        <location filename="../messagedialog/messagedialog.ui" line="143"/>
+        <source>Details...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.ui" line="202"/>
+        <source>DETAILS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.ui" line="219"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="100"/>
+        <location filename="../messagedialog/messagedialog.cpp" line="129"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="101"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="102"/>
+        <source>Save All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="103"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="104"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="105"/>
+        <source>Yes to All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="106"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="107"/>
+        <source>No to All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="108"/>
+        <source>Abort</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="109"/>
+        <source>Retry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="110"/>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="111"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="112"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="113"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="114"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="115"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="116"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../messagedialog/messagedialog.cpp" line="117"/>
+        <source>Restore Defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
