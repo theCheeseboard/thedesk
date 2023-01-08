03ff98ec26e1934bc6b1cd738e07348dcc482a73
Parlance <parlance@vicr123.com>
2023-01-08 21:23:00 +0000

    Update Translations

---
+++ libthedesk/translations/cs_CZ.ts
@@ -0,0 +1,113 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="cs_CZ">
+  <context>
+    <name>KeyboardLayoutSelector</name>
+    <message>
+      <translation></translation>
+      <source>Select a keyboard layout</source>
+    </message>
+  </context>
+  <context>
+    <name>LocaleSelector</name>
+    <message>
+      <translation></translation>
+      <source>Select a locale</source>
+    </message>
+  </context>
+  <context>
+    <name>OnboardingFinal</name>
+    <message>
+      <translation></translation>
+      <source>That's everything!</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>We hope you enjoy using theDesk!</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Get Started</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Done</source>
+    </message>
+  </context>
+  <context>
+    <name>OnboardingVideo</name>
+    <message>
+      <translation></translation>
+      <source>qrc:/libthedesk/onboarding/OnboardingVideoForm.qml</source>
+    </message>
+  </context>
+  <context>
+    <name>OnboardingWelcome</name>
+    <message>
+      <translation></translation>
+      <source>Language</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Emergency</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Let's go!</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Welcome</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Welcome to %1!</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>You've made it! We'll quickly go through some important setup like setting up accounts and connecting to the Internet, and then you'll be up and running.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Just a bit of important setup to do before you get started. This won't take long!</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>OTHER OPTIONS</source>
+    </message>
+  </context>
+  <context>
+    <name>QuietModeManagerTd</name>
+    <message>
+      <translation></translation>
+      <source>Sound</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Critical Only</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>No Notifications</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Mute</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Allows all sounds</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Hides non-critical notifications</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Hides all notifications</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Mutes all sound altogether</source>
+    </message>
+  </context>
+</TS>
