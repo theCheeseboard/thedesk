416c215da05acc28a3919daf32dc90b78ffec174
Parlance <parlance@vicr123.com>
2023-08-15 22:03:18 +0000

    Update Translations - vi_VN
    Co-Authored-By: Victor Tran <vicr12345@gmail.com>

--- plugins/OverviewPlugin/translations/vi_VN.ts
+++ plugins/OverviewPlugin/translations/vi_VN.ts
@@ -1,63 +1,62 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="vi_VN" sourcelanguage="en_US">
-<context>
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="vi_VN">
+  <context>
     <name>OverviewPane</name>
     <message>
-        <location filename="../OverviewPane/overviewpane.cpp" line="109"/>
-        <source>Hi %1!</source>
-        <translation>Chào %1!</translation>
+      <translation>Chào %1!</translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="109" />
+      <source>Hi %1!</source>
     </message>
     <message>
-        <location filename="../OverviewPane/overviewpane.cpp" line="111"/>
-        <source>Good morning, %1!</source>
-        <translation>Chào buổi sáng, %1!</translation>
+      <translation>Chào buổi sáng, %1!</translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="111" />
+      <source>Good morning, %1!</source>
     </message>
     <message>
-        <location filename="../OverviewPane/overviewpane.cpp" line="113"/>
-        <source>Good afternoon, %1!</source>
-        <translation>Chào buổi chiều, %1!</translation>
+      <translation>Chào buổi chiều, %1!</translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="113" />
+      <source>Good afternoon, %1!</source>
     </message>
     <message>
-        <location filename="../OverviewPane/overviewpane.cpp" line="115"/>
-        <source>Good evening, %1!</source>
-        <translation>Chào buổi tối, %1!</translation>
+      <translation>Chào buổi tối, %1!</translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="115" />
+      <source>Good evening, %1!</source>
     </message>
     <message numerus="yes">
-        <location filename="../OverviewPane/overviewpane.cpp" line="208"/>
-        <source>Daylight Savings Time is ending on %1. The clock will automatically shift backwards by %n hour(s).</source>
-        <translation type="unfinished">
-            <numerusform>Giờ mùa hè sẽ kết thúc ngày %2. Đồng hồ sẽ tự động quay ngược %1 giờ. {1.?}</numerusform>
-        </translation>
+      <translation>
+        <numerusform>Giờ mùa hè sẽ kết thúc ngày %1. Đồng hồ sẽ tự động quay ngược %n giờ.</numerusform>
+      </translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="208" />
+      <source>Daylight Savings Time is ending on %1. The clock will automatically shift backwards by %n hour(s).</source>
     </message>
     <message numerus="yes">
-        <location filename="../OverviewPane/overviewpane.cpp" line="210"/>
-        <source>Daylight Savings Time is starting on %1. The clock will automatically shift forwards by %n hour(s).</source>
-        <translation type="unfinished">
-            <numerusform>Giờ mùa hè sẽ bắt đầu ngày %2. Đồng hồ sẽ tự động tua đi %1 giờ. {1.?}</numerusform>
-        </translation>
+      <translation>
+        <numerusform>Giờ mùa hè sẽ bắt đầu ngày %1. Đồng hồ sẽ tự động tua đi %n giờ.</numerusform>
+      </translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="210" />
+      <source>Daylight Savings Time is starting on %1. The clock will automatically shift forwards by %n hour(s).</source>
     </message>
     <message numerus="yes">
-        <source>Daylight Savings Time is ending on %2. The clock will automatically shift backwards by %n hour(s).</source>
-        <translation type="vanished">
-            <numerusform>Giờ mùa hè sẽ kết thúc ngày %2. Đồng hồ sẽ tự động quay ngược %1 giờ.</numerusform>
-        </translation>
+      <translation>
+        <numerusform>Giờ mùa hè sẽ kết thúc ngày %2. Đồng hồ sẽ tự động quay ngược %n giờ.</numerusform>
+      </translation>
+      <source>Daylight Savings Time is ending on %2. The clock will automatically shift backwards by %n hour(s).</source>
     </message>
     <message numerus="yes">
-        <source>Daylight Savings Time is starting on %2. The clock will automatically shift forwards by %n hour(s).</source>
-        <translation type="vanished">
-            <numerusform>Giờ mùa hè sẽ bắt đầu ngày %2. Đồng hồ sẽ tự động tua đi %1 giờ.</numerusform>
-        </translation>
+      <translation>
+        <numerusform>Giờ mùa hè sẽ bắt đầu ngày %2. Đồng hồ sẽ tự động tua đi %n giờ.</numerusform>
+      </translation>
+      <source>Daylight Savings Time is starting on %2. The clock will automatically shift forwards by %n hour(s).</source>
     </message>
     <message>
-        <location filename="../OverviewPane/overviewpane.cpp" line="342"/>
-        <source>Overview</source>
-        <translation>Tổng quan</translation>
+      <translation>Tổng quan</translation>
+      <location filename="../OverviewPane/overviewpane.cpp" line="342" />
+      <source>Overview</source>
     </message>
     <message>
-        <location filename="../OverviewPane/overviewpane.ui" line="102"/>
-        <source>Hope you&apos;re having a great day. Here&apos;s what we&apos;ve got for you:</source>
-        <translation>Chúng tôi hy vọng ngày nay tuyệt vời. Chúng tôi có điều này cho bạn:</translation>
+      <translation>Chúng tôi hy vọng ngày nay tuyệt vời. Chúng tôi có điều này cho bạn:</translation>
+      <location filename="../OverviewPane/overviewpane.ui" line="102" />
+      <source>Hope you're having a great day. Here's what we've got for you:</source>
     </message>
-</context>
+  </context>
 </TS>
